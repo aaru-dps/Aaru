168b22f86912a9291c811f5458f5319d06826375
Natalia Portillo <claunia@claunia.com>
2014-08-25 17:20:05 +0100

    Added static CCITT-CRC16 lookup table. 	Subchannel Q is big-endian. 	Detect if a CD-Text pack is really a CD-Text pack to not 	confuse with CD+G packs. 	An audio disc can have subchannel correct but channel null. 	Viceversa can happen. Those should be considered correct and 	not unknown.

--- DiscImageChef/Checksums/CDChecksums.cs
+++ DiscImageChef/Checksums/CDChecksums.cs
@@ -61,15 +61,20 @@ namespace DiscImageChef.Checksums
 
                         bool? channelStatus = CheckCDSectorChannel(channel);
                         bool? subchannelStatus = CheckCDSectorSubChannel(subchannel);
+                        bool? status = null;
 
-                        if (channelStatus == null || subchannelStatus == null)
-                            return null;
+                        if (channelStatus == null && subchannelStatus == null)
+                            status = null;
                         if (channelStatus == false || subchannelStatus == false)
-                            return false;
+                            status = false;
+                        if (channelStatus == null && subchannelStatus == true)
+                            status = true;
+                        if (channelStatus == true && subchannelStatus == null)
+                            status = true;
                         if (channelStatus == true && subchannelStatus == true)
-                            return true;
+                            status = true;
 
-                        return null;
+                        return status;
                     }
                 case 2352:
                     return CheckCDSectorChannel(buffer);
@@ -421,80 +426,130 @@ namespace DiscImageChef.Checksums
                     CDTextPack4[j] = (byte)(CDTextPack4[j] | (subchannel[i++] & 0x3F));
             }
 
-            UInt16 QSubChannelCRC = BitConverter.ToUInt16(QSubChannel, 10);
+            BigEndianBitConverter.IsLittleEndian = true;
+
+            UInt16 QSubChannelCRC = BigEndianBitConverter.ToUInt16(QSubChannel, 10);
             byte[] QSubChannelForCRC = new byte[10];
             Array.Copy(QSubChannel, 0, QSubChannelForCRC, 0, 10);
-            byte[] CalculatedQCRCBytes = new byte[2];
-            CRC16Context.Data(QSubChannelForCRC, out CalculatedQCRCBytes);
-            UInt16 CalculatedQCRC = BitConverter.ToUInt16(CalculatedQCRCBytes, 0);
+            UInt16 CalculatedQCRC = CalculateCCITT_CRC16(QSubChannelForCRC);
 
             if (QSubChannelCRC != CalculatedQCRC)
             {
                 if (MainClass.isDebug)
-//                    Console.WriteLine("DEBUG (CDChecksums): Q subchannel CRC 0x{0:X4}, expected 0x{1:X4}", CalculatedQCRC, QSubChannelCRC);
+                    Console.WriteLine("DEBUG (CDChecksums): Q subchannel CRC 0x{0:X4}, expected 0x{1:X4}", CalculatedQCRC, QSubChannelCRC);
                 status = false;
             }
 
-            UInt16 CDTextPack1CRC = BitConverter.ToUInt16(CDTextPack1, 16);
-            byte[] CDTextPack1ForCRC = new byte[16];
-            Array.Copy(CDTextPack1, 0, CDTextPack1ForCRC, 0, 16);
-            byte[] CalculatedCDTP1CRCBytes = new byte[2];
-            CRC16Context.Data(CDTextPack1ForCRC, out CalculatedCDTP1CRCBytes);
-            UInt16 CalculatedCDTP1CRC = BitConverter.ToUInt16(CalculatedCDTP1CRCBytes, 0);
-
-            if (CDTextPack1CRC != CalculatedCDTP1CRC && CDTextPack1CRC != 0)
+            if ((CDTextPack1[0] & 0x80) == 0x80)
             {
-                if (MainClass.isDebug)
-//                    Console.WriteLine("DEBUG (CDChecksums): CD-Text Pack 1 CRC 0x{0:X4}, expected 0x{1:X4}", CDTextPack1CRC, CalculatedCDTP1CRC);
+                UInt16 CDTextPack1CRC = BigEndianBitConverter.ToUInt16(CDTextPack1, 16);
+                byte[] CDTextPack1ForCRC = new byte[16];
+                Array.Copy(CDTextPack1, 0, CDTextPack1ForCRC, 0, 16);
+                UInt16 CalculatedCDTP1CRC = CalculateCCITT_CRC16(CDTextPack1ForCRC);
+
+                if (CDTextPack1CRC != CalculatedCDTP1CRC && CDTextPack1CRC != 0)
+                {
+                    if (MainClass.isDebug)
+                    Console.WriteLine("DEBUG (CDChecksums): CD-Text Pack 1 CRC 0x{0:X4}, expected 0x{1:X4}", CDTextPack1CRC, CalculatedCDTP1CRC);
                 status = false;
+                }
             }
 
-            UInt16 CDTextPack2CRC = BitConverter.ToUInt16(CDTextPack2, 16);
-            byte[] CDTextPack2ForCRC = new byte[16];
-            Array.Copy(CDTextPack2, 0, CDTextPack2ForCRC, 0, 16);
-            byte[] CalculatedCDTP2CRCBytes = new byte[2];
-            CRC16Context.Data(CDTextPack2ForCRC, out CalculatedCDTP2CRCBytes);
-            UInt16 CalculatedCDTP2CRC = BitConverter.ToUInt16(CalculatedCDTP2CRCBytes, 0);
-
-            if (CDTextPack2CRC != CalculatedCDTP2CRC && CDTextPack2CRC != 0)
+            if ((CDTextPack2[0] & 0x80) == 0x80)
             {
-                if (MainClass.isDebug)
-//                    Console.WriteLine("DEBUG (CDChecksums): CD-Text Pack 2 CRC 0x{0:X4}, expected 0x{1:X4}", CDTextPack2CRC, CalculatedCDTP2CRC);
-                status = false;
+                UInt16 CDTextPack2CRC = BigEndianBitConverter.ToUInt16(CDTextPack2, 16);
+                byte[] CDTextPack2ForCRC = new byte[16];
+                Array.Copy(CDTextPack2, 0, CDTextPack2ForCRC, 0, 16);
+                UInt16 CalculatedCDTP2CRC = CalculateCCITT_CRC16(CDTextPack2ForCRC);
+                Console.WriteLine("Cyclic CDTP2 0x{0:X4}, Calc CDTP2 0x{1:X4}", CDTextPack2CRC, CalculatedCDTP2CRC);
+
+                if (CDTextPack2CRC != CalculatedCDTP2CRC && CDTextPack2CRC != 0)
+                {
+                    if (MainClass.isDebug)
+                        Console.WriteLine("DEBUG (CDChecksums): CD-Text Pack 2 CRC 0x{0:X4}, expected 0x{1:X4}", CDTextPack2CRC, CalculatedCDTP2CRC);
+                    status = false;
+                }
             }
 
-            UInt16 CDTextPack3CRC = BitConverter.ToUInt16(CDTextPack3, 16);
-            byte[] CDTextPack3ForCRC = new byte[16];
-            Array.Copy(CDTextPack3, 0, CDTextPack3ForCRC, 0, 16);
-            byte[] CalculatedCDTP3CRCBytes = new byte[2];
-            CRC16Context.Data(CDTextPack3ForCRC, out CalculatedCDTP3CRCBytes);
-            UInt16 CalculatedCDTP3CRC = BitConverter.ToUInt16(CalculatedCDTP3CRCBytes, 0);
-
-            if (CDTextPack3CRC != CalculatedCDTP3CRC && CDTextPack3CRC != 0)
+            if ((CDTextPack3[0] & 0x80) == 0x80)
             {
-                if (MainClass.isDebug)
-//                    Console.WriteLine("DEBUG (CDChecksums): CD-Text Pack 3 CRC 0x{0:X4}, expected 0x{1:X4}", CDTextPack3CRC, CalculatedCDTP3CRC);
-                status = false;
+                UInt16 CDTextPack3CRC = BigEndianBitConverter.ToUInt16(CDTextPack3, 16);
+                byte[] CDTextPack3ForCRC = new byte[16];
+                Array.Copy(CDTextPack3, 0, CDTextPack3ForCRC, 0, 16);
+                UInt16 CalculatedCDTP3CRC = CalculateCCITT_CRC16(CDTextPack3ForCRC);
+                Console.WriteLine("Cyclic CDTP3 0x{0:X4}, Calc CDTP3 0x{1:X4}", CDTextPack3CRC, CalculatedCDTP3CRC);
+
+                if (CDTextPack3CRC != CalculatedCDTP3CRC && CDTextPack3CRC != 0)
+                {
+                    if (MainClass.isDebug)
+                        Console.WriteLine("DEBUG (CDChecksums): CD-Text Pack 3 CRC 0x{0:X4}, expected 0x{1:X4}", CDTextPack3CRC, CalculatedCDTP3CRC);
+                    status = false;
+                }
             }
 
-            UInt16 CDTextPack4CRC = BitConverter.ToUInt16(CDTextPack4, 16);
-            byte[] CDTextPack4ForCRC = new byte[16];
-            Array.Copy(CDTextPack4, 0, CDTextPack4ForCRC, 0, 16);
-            byte[] CalculatedCDTP4CRCBytes = new byte[2];
-            CRC16Context.Data(CDTextPack4ForCRC, out CalculatedCDTP4CRCBytes);
-            UInt16 CalculatedCDTP4CRC = BitConverter.ToUInt16(CalculatedCDTP4CRCBytes, 0);
-
-            if (CDTextPack4CRC != CalculatedCDTP4CRC && CDTextPack4CRC != 0)
+            if ((CDTextPack4[0] & 0x80) == 0x80)
             {
-                if (MainClass.isDebug)
-//                    Console.WriteLine("DEBUG (CDChecksums): CD-Text Pack 4 CRC 0x{0:X4}, expected 0x{1:X4}", CDTextPack4CRC, CalculatedCDTP4CRC);
-                status = false;
+                UInt16 CDTextPack4CRC = BigEndianBitConverter.ToUInt16(CDTextPack4, 16);
+                byte[] CDTextPack4ForCRC = new byte[16];
+                Array.Copy(CDTextPack4, 0, CDTextPack4ForCRC, 0, 16);
+                UInt16 CalculatedCDTP4CRC = CalculateCCITT_CRC16(CDTextPack4ForCRC);
+                Console.WriteLine("Cyclic CDTP4 0x{0:X4}, Calc CDTP4 0x{1:X4}", CDTextPack4CRC, CalculatedCDTP4CRC);
+
+                if (CDTextPack4CRC != CalculatedCDTP4CRC && CDTextPack4CRC != 0)
+                {
+                    if (MainClass.isDebug)
+                        Console.WriteLine("DEBUG (CDChecksums): CD-Text Pack 4 CRC 0x{0:X4}, expected 0x{1:X4}", CDTextPack4CRC, CalculatedCDTP4CRC);
+                    status = false;
+                }
             }
 
-//            return status;
-            // TODO: Correct CRC poly and seed
-            // TODO: Detect CD-Text vs CD+G packets
-            return null;
+            return status;
+        }
+
+        static readonly ushort[] CCITT_CRC16Table = {
+            0x0000,  0x1021,  0x2042,  0x3063,  0x4084,  0x50a5,  0x60c6,  0x70e7,
+            0x8108,  0x9129,  0xa14a,  0xb16b,  0xc18c,  0xd1ad,  0xe1ce,  0xf1ef,
+            0x1231,  0x0210,  0x3273,  0x2252,  0x52b5,  0x4294,  0x72f7,  0x62d6,
+            0x9339,  0x8318,  0xb37b,  0xa35a,  0xd3bd,  0xc39c,  0xf3ff,  0xe3de,
+            0x2462,  0x3443,  0x0420,  0x1401,  0x64e6,  0x74c7,  0x44a4,  0x5485,
+            0xa56a,  0xb54b,  0x8528,  0x9509,  0xe5ee,  0xf5cf,  0xc5ac,  0xd58d,
+            0x3653,  0x2672,  0x1611,  0x0630,  0x76d7,  0x66f6,  0x5695,  0x46b4,
+            0xb75b,  0xa77a,  0x9719,  0x8738,  0xf7df,  0xe7fe,  0xd79d,  0xc7bc,
+            0x48c4,  0x58e5,  0x6886,  0x78a7,  0x0840,  0x1861,  0x2802,  0x3823,
+            0xc9cc,  0xd9ed,  0xe98e,  0xf9af,  0x8948,  0x9969,  0xa90a,  0xb92b,
+            0x5af5,  0x4ad4,  0x7ab7,  0x6a96,  0x1a71,  0x0a50,  0x3a33,  0x2a12,
+            0xdbfd,  0xcbdc,  0xfbbf,  0xeb9e,  0x9b79,  0x8b58,  0xbb3b,  0xab1a,
+            0x6ca6,  0x7c87,  0x4ce4,  0x5cc5,  0x2c22,  0x3c03,  0x0c60,  0x1c41,
+            0xedae,  0xfd8f,  0xcdec,  0xddcd,  0xad2a,  0xbd0b,  0x8d68,  0x9d49,
+            0x7e97,  0x6eb6,  0x5ed5,  0x4ef4,  0x3e13,  0x2e32,  0x1e51,  0x0e70,
+            0xff9f,  0xefbe,  0xdfdd,  0xcffc,  0xbf1b,  0xaf3a,  0x9f59,  0x8f78,
+            0x9188,  0x81a9,  0xb1ca,  0xa1eb,  0xd10c,  0xc12d,  0xf14e,  0xe16f,
+            0x1080,  0x00a1,  0x30c2,  0x20e3,  0x5004,  0x4025,  0x7046,  0x6067,
+            0x83b9,  0x9398,  0xa3fb,  0xb3da,  0xc33d,  0xd31c,  0xe37f,  0xf35e,
+            0x02b1,  0x1290,  0x22f3,  0x32d2,  0x4235,  0x5214,  0x6277,  0x7256,
+            0xb5ea,  0xa5cb,  0x95a8,  0x8589,  0xf56e,  0xe54f,  0xd52c,  0xc50d,
+            0x34e2,  0x24c3,  0x14a0,  0x0481,  0x7466,  0x6447,  0x5424,  0x4405,
+            0xa7db,  0xb7fa,  0x8799,  0x97b8,  0xe75f,  0xf77e,  0xc71d,  0xd73c,
+            0x26d3,  0x36f2,  0x0691,  0x16b0,  0x6657,  0x7676,  0x4615,  0x5634,
+            0xd94c,  0xc96d,  0xf90e,  0xe92f,  0x99c8,  0x89e9,  0xb98a,  0xa9ab,
+            0x5844,  0x4865,  0x7806,  0x6827,  0x18c0,  0x08e1,  0x3882,  0x28a3,
+            0xcb7d,  0xdb5c,  0xeb3f,  0xfb1e,  0x8bf9,  0x9bd8,  0xabbb,  0xbb9a,
+            0x4a75,  0x5a54,  0x6a37,  0x7a16,  0x0af1,  0x1ad0,  0x2ab3,  0x3a92,
+            0xfd2e,  0xed0f,  0xdd6c,  0xcd4d,  0xbdaa,  0xad8b,  0x9de8,  0x8dc9,
+            0x7c26,  0x6c07,  0x5c64,  0x4c45,  0x3ca2,  0x2c83,  0x1ce0,  0x0cc1,
+            0xef1f,  0xff3e,  0xcf5d,  0xdf7c,  0xaf9b,  0xbfba,  0x8fd9,  0x9ff8,
+            0x6e17,  0x7e36,  0x4e55,  0x5e74,  0x2e93,  0x3eb2,  0x0ed1,  0x1ef0,
+        };
+
+        static ushort CalculateCCITT_CRC16(byte[] buffer)
+        {
+            UInt16 CRC16=0;
+            for (int i = 0; i < buffer.Length; i++) {
+                CRC16 = (ushort)(CCITT_CRC16Table[(CRC16 >> 8) ^ buffer[i]] ^ (CRC16 << 8));
+            }
+            CRC16 = (ushort)~CRC16;
+
+            return CRC16;
         }
     }
 }
