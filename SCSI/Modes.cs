e325bc848a5bbdc965402d81cf4a0f4253cbba70
Natalia Portillo <claunia@claunia.com>
2015-10-30 02:53:36 +0000

    Implemented decoding mode page 07h.

--- SCSI/Modes.cs
+++ SCSI/Modes.cs
@@ -2417,6 +2417,112 @@ namespace DiscImageChef.Decoders.SCSI
             return sb.ToString();
         }
         #endregion Mode Page 0x04: Rigid disk drive geometry page
+
+        #region Mode Page 0x07: Verify error recovery page
+        /// <summary>
+        /// Disconnect-reconnect page
+        /// Page code 0x07
+        /// 12 bytes in SCSI-2
+        /// </summary>
+        public struct ModePage_07
+        {
+            /// <summary>
+            /// Parameters can be saved
+            /// </summary>
+            public bool PS;
+            /// <summary>
+            /// Enable early recovery
+            /// </summary>
+            public bool EER;
+            /// <summary>
+            /// Post error reporting
+            /// </summary>
+            public bool PER;
+            /// <summary>
+            /// Disable transfer on error
+            /// </summary>
+            public bool DTE;
+            /// <summary>
+            /// Disable correction
+            /// </summary>
+            public bool DCR;
+            /// <summary>
+            /// How many times to retry a verify operation
+            /// </summary>
+            public byte VerifyRetryCount;
+            /// <summary>
+            /// How many bits of largest data burst error is maximum to apply error correction on it
+            /// </summary>
+            public byte CorrectionSpan;
+            /// <summary>
+            /// Maximum time in ms to use in data error recovery procedures
+            /// </summary>
+            public ushort RecoveryTimeLimit;
+        }
+
+        public static ModePage_07? DecodeModePage_07(byte[] pageResponse)
+        {
+            if (pageResponse == null)
+                return null;
+
+            if ((pageResponse[0] & 0x3F) != 0x07)
+                return null;
+
+            if (pageResponse[1] + 2 != pageResponse.Length)
+                return null;
+
+            if (pageResponse.Length < 12)
+                return null;
+
+            ModePage_07 decoded = new ModePage_07();
+
+            decoded.PS |= (pageResponse[0] & 0x80) == 0x80;
+            decoded.EER |= (pageResponse[2] & 0x08) == 0x08;
+            decoded.PER |= (pageResponse[2] & 0x04) == 0x04;
+            decoded.DTE |= (pageResponse[2] & 0x02) == 0x02;
+            decoded.DCR |= (pageResponse[2] & 0x01) == 0x01;
+
+            decoded.VerifyRetryCount = pageResponse[3];
+            decoded.CorrectionSpan = pageResponse[4];
+            decoded.RecoveryTimeLimit = (ushort)((pageResponse[10] << 8) + pageResponse[11]);
+
+            return decoded;
+        }
+
+        public static string PrettifyModePage_07(byte[] pageResponse)
+        {
+            return PrettifyModePage_07(DecodeModePage_07(pageResponse));
+        }
+
+        public static string PrettifyModePage_07(ModePage_07? modePage)
+        {
+            if (!modePage.HasValue)
+                return null;
+
+            ModePage_07 page = modePage.Value;
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SCSI Verify error recovery page:");
+
+            if (page.PS)
+                sb.AppendLine("\tParameters can be saved");
+
+            if (page.EER)
+                sb.AppendLine("\tDrive will use the most expedient form of error recovery first");
+            if (page.PER)
+                sb.AppendLine("\tDrive shall report recovered errors");
+            if (page.DTE)
+                sb.AppendLine("\tTransfer will be terminated upon error detection");
+            if (page.DCR)
+                sb.AppendLine("\tError correction is disabled");
+            if (page.VerifyRetryCount > 0)
+                sb.AppendFormat("\tDrive will repeat verify operations {0} times", page.VerifyRetryCount).AppendLine();
+            if (page.RecoveryTimeLimit > 0)
+                sb.AppendFormat("\tDrive will employ a maximum of {0} ms to recover data", page.RecoveryTimeLimit).AppendLine();
+
+            return sb.ToString();
+        }
+        #endregion Mode Page 0x07: Verify error recovery page
     }
 }
 
