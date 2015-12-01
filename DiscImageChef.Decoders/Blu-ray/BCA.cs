5bfcc5acbec98f23678fd0c58aa5c00ad6a05172
Natalia Portillo <claunia@claunia.com>
2015-12-01 03:23:31 +0000

    Separated blu-ray structure decoders.

---
+++ DiscImageChef.Decoders/Blu-ray/BCA.cs
@@ -0,0 +1,141 @@
+﻿// /***************************************************************************
+// The Disc Image Chef
+// ----------------------------------------------------------------------------
+//
+// Filename       : BCA.cs
+// Version        : 1.0
+// Author(s)      : Natalia Portillo
+//
+// Component      : Component
+//
+// Revision       : $Revision$
+// Last change by : $Author$
+// Date           : $Date$
+//
+// --[ Description ] ----------------------------------------------------------
+//
+// Description
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright (C) 2011-2015 Claunia.com
+// ****************************************************************************/
+// //$Id$
+using System;
+using DiscImageChef.Console;
+using System.Text;
+
+namespace DiscImageChef.Decoders.Bluray
+{
+    /// <summary>
+    /// Information from the following standards:
+    /// ANSI X3.304-1997
+    /// T10/1048-D revision 9.0
+    /// T10/1048-D revision 10a
+    /// T10/1228-D revision 7.0c
+    /// T10/1228-D revision 11a
+    /// T10/1363-D revision 10g
+    /// T10/1545-D revision 1d
+    /// T10/1545-D revision 5
+    /// T10/1545-D revision 5a
+    /// T10/1675-D revision 2c
+    /// T10/1675-D revision 4
+    /// T10/1836-D revision 2g
+    /// </summary>
+    public static class BCA
+    {
+        #region Public methods
+        public static BurstCuttingArea? Decode(byte[] BCAResponse)
+        {
+            if (BCAResponse == null)
+                return null;
+
+            if (BCAResponse.Length != 68)
+            {
+                DicConsole.DebugWriteLine("BD BCA decoder", "Found incorrect Blu-ray BCA size ({0} bytes)", BCAResponse.Length);
+                return null;
+            }
+
+            BurstCuttingArea decoded = new BurstCuttingArea();
+
+            BigEndianBitConverter.IsLittleEndian = BitConverter.IsLittleEndian;
+
+            decoded.DataLength = BigEndianBitConverter.ToUInt16(BCAResponse, 0);
+            decoded.Reserved1 = BCAResponse[2];
+            decoded.Reserved2 = BCAResponse[3];
+            decoded.BCA = new byte[64];
+            Array.Copy(BCAResponse, 4, decoded.BCA, 0, 64);
+
+            return decoded;
+        }
+
+        public static string Prettify(BurstCuttingArea? BCAResponse)
+        {
+            if (BCAResponse == null)
+                return null;
+
+            BurstCuttingArea response = BCAResponse.Value;
+
+            StringBuilder sb = new StringBuilder();
+
+            #if DEBUG
+            if(response.Reserved1 != 0)
+                sb.AppendFormat("Reserved1 = 0x{0:X2}", response.Reserved1).AppendLine();
+            if(response.Reserved2 != 0)
+                sb.AppendFormat("Reserved2 = 0x{0:X2}", response.Reserved2).AppendLine();
+            #endif
+
+            sb.AppendFormat("Blu-ray Burst Cutting Area in hex follows:");
+            sb.AppendLine(PrintHex.ByteArrayToHexArrayString(response.BCA, 80));
+
+            return sb.ToString();
+        }
+
+        public static string Prettify(byte[] BCAResponse)
+        {
+            return Prettify(Decode(BCAResponse));
+        }
+        #endregion Public methods
+
+        #region Public structures
+        public struct BurstCuttingArea
+        {
+            /// <summary>
+            /// Bytes 0 to 1
+            /// Always 66
+            /// </summary>
+            public UInt16 DataLength;
+            /// <summary>
+            /// Byte 2
+            /// Reserved
+            /// </summary>
+            public byte Reserved1;
+            /// <summary>
+            /// Byte 3
+            /// Reserved
+            /// </summary>
+            public byte Reserved2;
+            /// <summary>
+            /// Byte 4 to 67
+            /// BCA data
+            /// </summary>
+            public byte[] BCA;
+        }
+        #endregion Public structures
+    }
+}
+
---
+++ DiscImageChef.Decoders/Blu-ray/Cartridge.cs
@@ -0,0 +1,208 @@
+﻿// /***************************************************************************
+// The Disc Image Chef
+// ----------------------------------------------------------------------------
+//
+// Filename       : Cartridge.cs
+// Version        : 1.0
+// Author(s)      : Natalia Portillo
+//
+// Component      : Component
+//
+// Revision       : $Revision$
+// Last change by : $Author$
+// Date           : $Date$
+//
+// --[ Description ] ----------------------------------------------------------
+//
+// Description
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright (C) 2011-2015 Claunia.com
+// ****************************************************************************/
+// //$Id$
+using System;
+using DiscImageChef.Console;
+using System.Text;
+
+namespace DiscImageChef.Decoders.Bluray
+{
+    /// <summary>
+    /// Information from the following standards:
+    /// ANSI X3.304-1997
+    /// T10/1048-D revision 9.0
+    /// T10/1048-D revision 10a
+    /// T10/1228-D revision 7.0c
+    /// T10/1228-D revision 11a
+    /// T10/1363-D revision 10g
+    /// T10/1545-D revision 1d
+    /// T10/1545-D revision 5
+    /// T10/1545-D revision 5a
+    /// T10/1675-D revision 2c
+    /// T10/1675-D revision 4
+    /// T10/1836-D revision 2g
+    /// </summary>
+    public static class Cartridge
+    {
+        #region Public methods
+        public static CartridgeStatus? Decode(byte[] CSResponse)
+        {
+            if (CSResponse == null)
+                return null;
+
+            if (CSResponse.Length != 8)
+            {
+                DicConsole.DebugWriteLine("BD Cartridge Status decoder", "Found incorrect Blu-ray Cartridge Status size ({0} bytes)", CSResponse.Length);
+                return null;
+            }
+
+            CartridgeStatus decoded = new CartridgeStatus();
+
+            BigEndianBitConverter.IsLittleEndian = BitConverter.IsLittleEndian;
+
+            decoded.DataLength = BigEndianBitConverter.ToUInt16(CSResponse, 0);
+            decoded.Reserved1 = CSResponse[2];
+            decoded.Reserved2 = CSResponse[3];
+            decoded.Cartridge = Convert.ToBoolean(CSResponse[4] & 0x80);
+            decoded.OUT = Convert.ToBoolean(CSResponse[4]&0x40);
+            decoded.Reserved3 = (byte)((CSResponse[4] & 0x38) >> 3);
+            decoded.OUT = Convert.ToBoolean(CSResponse[4]&0x04);
+            decoded.Reserved4 = (byte)(CSResponse[4] & 0x03);
+            decoded.Reserved5 = CSResponse[5];
+            decoded.Reserved6 = CSResponse[6];
+            decoded.Reserved7 = CSResponse[7];
+
+            return decoded;
+        }
+
+        public static string Prettify(CartridgeStatus? CSResponse)
+        {
+            if (CSResponse == null)
+                return null;
+
+            CartridgeStatus response = CSResponse.Value;
+
+            StringBuilder sb = new StringBuilder();
+
+            #if DEBUG
+            if(response.Reserved1 != 0)
+                sb.AppendFormat("Reserved1 = 0x{0:X2}", response.Reserved1).AppendLine();
+            if(response.Reserved2 != 0)
+                sb.AppendFormat("Reserved2 = 0x{0:X2}", response.Reserved2).AppendLine();
+            if(response.Reserved3 != 0)
+                sb.AppendFormat("Reserved3 = 0x{0:X8}", response.Reserved3).AppendLine();
+            if(response.Reserved4 != 0)
+                sb.AppendFormat("Reserved4 = 0x{0:X8}", response.Reserved4).AppendLine();
+            if(response.Reserved5 != 0)
+                sb.AppendFormat("Reserved5 = 0x{0:X8}", response.Reserved5).AppendLine();
+            if(response.Reserved6 != 0)
+                sb.AppendFormat("Reserved6 = 0x{0:X8}", response.Reserved6).AppendLine();
+            if(response.Reserved7 != 0)
+                sb.AppendFormat("Reserved7 = 0x{0:X8}", response.Reserved7).AppendLine();
+            #endif
+
+            if (response.Cartridge)
+            {
+                sb.AppendLine("Media is inserted in a cartridge");
+                if (response.OUT)
+                    sb.AppendLine("Media has been taken out, or inserted in, the cartridge");
+                if (response.CWP)
+                    sb.AppendLine("Media is write protected");
+            }
+            else
+            {
+                sb.AppendLine("Media is not in a cartridge");
+
+                #if DEBUG
+                if (response.OUT)
+                    sb.AppendLine("Media has out bit marked, shouldn't");
+                if (response.CWP)
+                    sb.AppendLine("Media has write protection bit marked, shouldn't");
+                #endif
+            }
+            return sb.ToString();
+        }
+
+        public static string Prettify(byte[] CSResponse)
+        {
+            return Prettify(Decode(CSResponse));
+        }
+        #endregion Public methods
+
+        #region Public structures
+        public struct CartridgeStatus
+        {
+            /// <summary>
+            /// Bytes 0 to 1
+            /// Always 6
+            /// </summary>
+            public UInt16 DataLength;
+            /// <summary>
+            /// Byte 2
+            /// Reserved
+            /// </summary>
+            public byte Reserved1;
+            /// <summary>
+            /// Byte 3
+            /// Reserved
+            /// </summary>
+            public byte Reserved2;
+            /// <summary>
+            /// Byte 4, bit 7
+            /// Medium is inserted in a cartridge
+            /// </summary>
+            public bool Cartridge;
+            /// <summary>
+            /// Byte 4, bit 6
+            /// Medium taken out / put in a cartridge
+            /// </summary>
+            public bool OUT;
+            /// <summary>
+            /// Byte 4, bits 5 to 3
+            /// Reserved
+            /// </summary>
+            public byte Reserved3;
+            /// <summary>
+            /// Byte 4, bit 2
+            /// Cartridge sets write protection
+            /// </summary>
+            public bool CWP;
+            /// <summary>
+            /// Byte 4, bits 1 to 0
+            /// Reserved
+            /// </summary>
+            public byte Reserved4;
+            /// <summary>
+            /// Byte 5
+            /// Reserved
+            /// </summary>
+            public byte Reserved5;
+            /// <summary>
+            /// Byte 6
+            /// Reserved
+            /// </summary>
+            public byte Reserved6;
+            /// <summary>
+            /// Byte 7
+            /// Reserved
+            /// </summary>
+            public byte Reserved7;
+        }
+        #endregion Public structures
+    }
+}
+
