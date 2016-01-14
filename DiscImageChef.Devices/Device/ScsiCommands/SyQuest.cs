afdcc5b7293c3d238fe814d1be6464e5ce1ee4cf
Natalia Portillo <claunia@claunia.com>
2016-01-14 20:40:45 +0000

    Added SyQuest vendor commands.

---
+++ DiscImageChef.Devices/Device/ScsiCommands/SyQuest.cs
@@ -0,0 +1,212 @@
+﻿// /***************************************************************************
+// The Disc Image Chef
+// ----------------------------------------------------------------------------
+//
+// Filename       : SyQuest.cs
+// Version        : 1.0
+// Author(s)      : Natalia Portillo
+//
+// Component      : Component
+//
+// Revision       : $Revision$
+// Last change by : $Author$
+// Date           : $Date$
+//
+// --[ Description ] ----------------------------------------------------------
+//
+// Description
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright (C) 2011-2015 Claunia.com
+// ****************************************************************************/
+// //$Id$
+using DiscImageChef.Console;
+
+namespace DiscImageChef.Devices
+{
+    public partial class Device
+    {
+        /// <summary>
+        /// Sends the SyQuest READ (6) command
+        /// </summary>
+        /// <returns><c>true</c> if the command failed and <paramref name="senseBuffer"/> contains the sense buffer.</returns>
+        /// <param name="buffer">Buffer where the SCSI READ response will be stored</param>
+        /// <param name="senseBuffer">Sense buffer.</param>
+        /// <param name="timeout">Timeout in seconds.</param>
+        /// <param name="duration">Duration in milliseconds it took for the device to execute the command.</param>
+        /// <param name="lba">Starting block.</param>
+        /// <param name="blockSize">Block size in bytes.</param>
+        public bool SyQuestRead6(out byte[] buffer, out byte[] senseBuffer, uint lba, uint blockSize, uint timeout, out double duration)
+        {
+            return SyQuestRead6(out buffer, out senseBuffer, lba, blockSize, 1, false, false, timeout, out duration);
+        }
+
+        /// <summary>
+        /// Sends the SyQuest READ LONG (6) command
+        /// </summary>
+        /// <returns><c>true</c> if the command failed and <paramref name="senseBuffer"/> contains the sense buffer.</returns>
+        /// <param name="buffer">Buffer where the SCSI READ response will be stored</param>
+        /// <param name="senseBuffer">Sense buffer.</param>
+        /// <param name="timeout">Timeout in seconds.</param>
+        /// <param name="duration">Duration in milliseconds it took for the device to execute the command.</param>
+        /// <param name="lba">Starting block.</param>
+        /// <param name="blockSize">Block size in bytes.</param>
+        public bool SyQuestReadLong6(out byte[] buffer, out byte[] senseBuffer, uint lba, uint blockSize, uint timeout, out double duration)
+        {
+            return SyQuestRead6(out buffer, out senseBuffer, lba, blockSize, 1, false, true, timeout, out duration);
+        }
+
+        /// <summary>
+        /// Sends the SyQuest READ (6) command
+        /// </summary>
+        /// <returns><c>true</c> if the command failed and <paramref name="senseBuffer"/> contains the sense buffer.</returns>
+        /// <param name="buffer">Buffer where the SCSI READ response will be stored</param>
+        /// <param name="senseBuffer">Sense buffer.</param>
+        /// <param name="timeout">Timeout in seconds.</param>
+        /// <param name="duration">Duration in milliseconds it took for the device to execute the command.</param>
+        /// <param name="lba">Starting block.</param>
+        /// <param name="inhibitDma">If set to <c>true</c>, block will not be transfer and will reside in the drive's buffer.</param>
+        /// <param name="readLong">If set to <c>true</c> drive will return ECC bytes and disable error detection.</param>
+        /// <param name="blockSize">Block size in bytes.</param>
+        /// <param name="transferLength">How many blocks to read.</param>
+        public bool SyQuestRead6(out byte[] buffer, out byte[] senseBuffer, uint lba, uint blockSize, byte transferLength, bool inhibitDma, bool readLong, uint timeout, out double duration)
+        {
+            senseBuffer = new byte[32];
+            byte[] cdb = new byte[6];
+            bool sense;
+
+            cdb[0] = (byte)ScsiCommands.Read6;
+            cdb[1] = (byte)((lba & 0x1F0000) >> 16);
+            cdb[2] = (byte)((lba & 0xFF00) >> 8);
+            cdb[3] = (byte)(lba & 0xFF);
+            cdb[4] = transferLength;
+            if (inhibitDma)
+                cdb[5] += 0x80;
+            if (readLong)
+                cdb[5] += 0x40;
+
+            if (!inhibitDma && !readLong)
+            {
+                if (transferLength == 0)
+                    buffer = new byte[256 * blockSize];
+                else
+                    buffer = new byte[transferLength * blockSize];
+            }
+            else if(readLong)
+            {
+                buffer = new byte[blockSize];
+                cdb[4] = 1;
+            }
+            else
+                buffer = new byte[0];
+
+            if (!inhibitDma)
+                lastError = SendScsiCommand(cdb, ref buffer, out senseBuffer, timeout, ScsiDirection.In, out duration, out sense);
+            else
+                lastError = SendScsiCommand(cdb, ref buffer, out senseBuffer, timeout, ScsiDirection.None, out duration, out sense);
+            
+            error = lastError != 0;
+
+            DicConsole.DebugWriteLine("SCSI Device", "SYQUEST READ (6) took {0} ms.", duration);
+
+            return sense;
+        }
+
+        /// <summary>
+        /// Requests the usage, seek and error counters, and resets them
+        /// </summary>
+        /// <param name="buffer">Buffer.</param>
+        /// <param name="senseBuffer">Sense buffer.</param>
+        /// <param name="timeout">Timeout.</param>
+        /// <param name="duration">Duration.</param>
+        public bool SyQuestReadUsageCounter(out byte[] buffer, out byte[] senseBuffer, uint timeout, out double duration)
+        {
+            return AdaptecReadUsageCounter(out buffer, out senseBuffer, false, timeout, duration);
+        }
+
+        /// <summary>
+        /// Sends the SyQuest READ LONG (10) command
+        /// </summary>
+        /// <returns><c>true</c> if the command failed and <paramref name="senseBuffer"/> contains the sense buffer.</returns>
+        /// <param name="buffer">Buffer where the SCSI READ response will be stored</param>
+        /// <param name="senseBuffer">Sense buffer.</param>
+        /// <param name="timeout">Timeout in seconds.</param>
+        /// <param name="duration">Duration in milliseconds it took for the device to execute the command.</param>
+        /// <param name="lba">Starting block.</param>
+        /// <param name="blockSize">Block size in bytes.</param>
+        public bool SyQuestReadLong10(out byte[] buffer, out byte[] senseBuffer, uint lba, uint blockSize, uint timeout, out double duration)
+        {
+            return SyQuestRead10(out buffer, out senseBuffer, lba, blockSize, 1, false, true, timeout, out duration);
+        }
+
+        /// <summary>
+        /// Sends the SyQuest READ (10) command
+        /// </summary>
+        /// <returns><c>true</c> if the command failed and <paramref name="senseBuffer"/> contains the sense buffer.</returns>
+        /// <param name="buffer">Buffer where the SCSI READ response will be stored</param>
+        /// <param name="senseBuffer">Sense buffer.</param>
+        /// <param name="timeout">Timeout in seconds.</param>
+        /// <param name="duration">Duration in milliseconds it took for the device to execute the command.</param>
+        /// <param name="lba">Starting block.</param>
+        /// <param name="inhibitDma">If set to <c>true</c>, block will not be transfer and will reside in the drive's buffer.</param>
+        /// <param name="readLong">If set to <c>true</c> drive will return ECC bytes and disable error detection.</param>
+        /// <param name="blockSize">Block size in bytes.</param>
+        /// <param name="transferLength">How many blocks to read.</param>
+        public bool SyQuestRead10(out byte[] buffer, out byte[] senseBuffer, uint lba, uint blockSize, ushort transferLength, bool inhibitDma, bool readLong, uint timeout, out double duration)
+        {
+            senseBuffer = new byte[32];
+            byte[] cdb = new byte[10];
+            bool sense;
+
+            cdb[0] = (byte)ScsiCommands.Read10;
+            cdb[2] = (byte)((lba & 0xFF000000) >> 24);
+            cdb[3] = (byte)((lba & 0xFF0000) >> 16);
+            cdb[4] = (byte)((lba & 0xFF00) >> 8);
+            cdb[5] = (byte)(lba & 0xFF);
+            cdb[7] = (byte)((transferLength & 0xFF00) >> 8);
+            cdb[8] = (byte)(transferLength & 0xFF);
+            if (inhibitDma)
+                cdb[9] += 0x80;
+            if (readLong)
+                cdb[9] += 0x40;
+
+            if (!inhibitDma && !readLong)
+            {
+                buffer = new byte[transferLength * blockSize];
+            }
+            else if(readLong)
+            {
+                buffer = new byte[blockSize];
+                cdb[4] = 1;
+            }
+            else
+                buffer = new byte[0];
+
+            if (!inhibitDma)
+                lastError = SendScsiCommand(cdb, ref buffer, out senseBuffer, timeout, ScsiDirection.In, out duration, out sense);
+            else
+                lastError = SendScsiCommand(cdb, ref buffer, out senseBuffer, timeout, ScsiDirection.None, out duration, out sense);
+            error = lastError != 0;
+
+            DicConsole.DebugWriteLine("SCSI Device", "SYQUEST READ (10) took {0} ms.", duration);
+
+            return sense;
+        }
+    }
+}
+
