fbf8ff18c90abe370aced0ed5909e0d8496397df
Natalia Portillo <claunia@claunia.com>
2016-07-28 22:25:26 +0100

    Refactor and code cleanup.

--- MediaType.cs
+++ MediaType.cs
@@ -30,8 +30,6 @@
 // Copyright © 2011-2016 Natalia Portillo
 // ****************************************************************************/
 
-using System;
-
 namespace DiscImageChef.CommonTypes
 {
     // Media (disk, cartridge, tape, cassette, etc) types
