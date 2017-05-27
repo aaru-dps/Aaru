a722547b5cdc3c60017acac51e0af4d74943c43a
Natalia Portillo <claunia@claunia.com>
2017-05-27 20:13:34 +0100

    Refactor: Move filesystem identification code to Core.

---
+++ DiscImageChef.Core/Filesystems.cs
@@ -0,0 +1,60 @@
+﻿// /***************************************************************************
+// The Disc Image Chef
+// ----------------------------------------------------------------------------
+//
+// Filename       : Filesystems.cs
+// Version        : 1.0
+// Author(s)      : Natalia Portillo
+//
+// Component      : Component
+//
+// Revision       : $Revision$
+// Last change by : $Author$
+// Date           : $Date$
+//
+// --[ Description ] ----------------------------------------------------------
+//
+// Description
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright (C) 2011-2015 Claunia.com
+// ****************************************************************************/
+// //$Id$
+using System;
+using System.Collections.Generic;
+using DiscImageChef.Filesystems;
+using DiscImageChef.ImagePlugins;
+
+namespace DiscImageChef.Core
+{
+    public static class Filesystems
+    {
+        public static void Identify(ImagePlugin imagePlugin, out List<string> id_plugins, ulong partitionStart, ulong partitionEnd)
+        {
+            id_plugins = new List<string>();
+            PluginBase plugins = new PluginBase();
+            plugins.RegisterAllPlugins();
+
+            foreach(Filesystem _plugin in plugins.PluginsList.Values)
+            {
+                if(_plugin.Identify(imagePlugin, partitionStart, partitionEnd))
+                    id_plugins.Add(_plugin.Name.ToLower());
+            }
+        }
+    }
+}
