0cd85170cad2a6ebdb467ed590508dafc3a772e4
Natalia Portillo <claunia@claunia.com>
2017-09-08 21:26:19 +0100

    Added tests for NEC vendor commands.

--- DiscImageChef.Tests.Devices/SCSI/NEC.cs
+++ DiscImageChef.Tests.Devices/SCSI/NEC.cs
@@ -66,11 +66,154 @@ namespace DiscImageChef.Tests.Devices.SCSI
                     case 0:
                         DicConsole.WriteLine("Returning to SCSI commands menu...");
                         return;
+                    case 1:
+                        ReadCdDa(devPath, dev);
+                        continue;
                     default:
                         DicConsole.WriteLine("Incorrect option. Press any key to continue...");
                         System.Console.ReadKey();
                         continue;
                 }
+            }
+        }
+
+        static void ReadCdDa(string devPath, Device dev)
+        {
+            uint address = 0;
+            uint length = 1;
+            string strDev;
+            int item;
+
+        parameters:
+            while(true)
+            {
+                System.Console.Clear();
+                DicConsole.WriteLine("Device: {0}", devPath);
+                DicConsole.WriteLine("Parameters for READ CD-DA command:");
+                DicConsole.WriteLine("LBA: {0}", address);
+                DicConsole.WriteLine("Will transfer {0} sectors", length);
+                DicConsole.WriteLine();
+                DicConsole.WriteLine("Choose what to do:");
+                DicConsole.WriteLine("1.- Change parameters.");
+                DicConsole.WriteLine("2.- Send command with these parameters.");
+                DicConsole.WriteLine("0.- Return to NEC vendor commands menu.");
+
+                strDev = System.Console.ReadLine();
+                if(!int.TryParse(strDev, out item))
+                {
+                    DicConsole.WriteLine("Not a number. Press any key to continue...");
+                    System.Console.ReadKey();
+                    continue;
+                }
+
+                switch(item)
+                {
+                    case 0:
+                        DicConsole.WriteLine("Returning to NEC vendor commands menu...");
+                        return;
+                    case 1:
+                        DicConsole.Write("Logical Block Address?: ");
+                        strDev = System.Console.ReadLine();
+                        if(!uint.TryParse(strDev, out address))
+                        {
+                            DicConsole.WriteLine("Not a number. Press any key to continue...");
+                            address = 0;
+                            System.Console.ReadKey();
+                            continue;
+                        }
+                        DicConsole.Write("How many sectors to transfer?: ");
+                        strDev = System.Console.ReadLine();
+                        if(!uint.TryParse(strDev, out length))
+                        {
+                            DicConsole.WriteLine("Not a number. Press any key to continue...");
+                            length = 1;
+                            System.Console.ReadKey();
+                            continue;
+                        }
+                        break;
+                    case 2:
+                        goto start;
+                }
+            }
+
+        start:
+            System.Console.Clear();
+            bool sense = dev.NecReadCdDa(out byte[] buffer, out byte[] senseBuffer, address, length, dev.Timeout, out double duration);
+
+        menu:
+            DicConsole.WriteLine("Device: {0}", devPath);
+            DicConsole.WriteLine("Sending READ CD-DA to the device:");
+            DicConsole.WriteLine("Command took {0} ms.", duration);
+            DicConsole.WriteLine("Sense is {0}.", sense);
+            DicConsole.WriteLine("Buffer is {0} bytes.", buffer == null ? "null" : buffer.Length.ToString());
+            DicConsole.WriteLine("Buffer is null or empty? {0}", ArrayHelpers.ArrayIsNullOrEmpty(buffer));
+            DicConsole.WriteLine("Sense buffer is {0} bytes.", senseBuffer == null ? "null" : senseBuffer.Length.ToString());
+            DicConsole.WriteLine("Sense buffer is null or empty? {0}", ArrayHelpers.ArrayIsNullOrEmpty(senseBuffer));
+            DicConsole.WriteLine();
+            DicConsole.WriteLine("Choose what to do:");
+            DicConsole.WriteLine("1.- Print buffer.");
+            DicConsole.WriteLine("2.- Print sense buffer.");
+            DicConsole.WriteLine("3.- Decode sense buffer.");
+            DicConsole.WriteLine("4.- Send command again.");
+            DicConsole.WriteLine("5.- Change parameters.");
+            DicConsole.WriteLine("0.- Return to NEC vendor commands menu.");
+            DicConsole.Write("Choose: ");
+
+            strDev = System.Console.ReadLine();
+            if(!int.TryParse(strDev, out item))
+            {
+                DicConsole.WriteLine("Not a number. Press any key to continue...");
+                System.Console.ReadKey();
+                System.Console.Clear();
+                goto menu;
+            }
+
+            switch(item)
+            {
+                case 0:
+                    DicConsole.WriteLine("Returning to NEC vendor commands menu...");
+                    return;
+                case 1:
+                    System.Console.Clear();
+                    DicConsole.WriteLine("Device: {0}", devPath);
+                    DicConsole.WriteLine("READ CD-DA response:");
+                    if(buffer != null)
+                        PrintHex.PrintHexArray(buffer, 64);
+                    DicConsole.WriteLine("Press any key to continue...");
+                    System.Console.ReadKey();
+                    System.Console.Clear();
+                    DicConsole.WriteLine("Device: {0}", devPath);
+                    goto menu;
+                case 2:
+                    System.Console.Clear();
+                    DicConsole.WriteLine("Device: {0}", devPath);
+                    DicConsole.WriteLine("READ CD-DA sense:");
+                    if(senseBuffer != null)
+                        PrintHex.PrintHexArray(senseBuffer, 64);
+                    DicConsole.WriteLine("Press any key to continue...");
+                    System.Console.ReadKey();
+                    System.Console.Clear();
+                    DicConsole.WriteLine("Device: {0}", devPath);
+                    goto menu;
+                case 3:
+                    System.Console.Clear();
+                    DicConsole.WriteLine("Device: {0}", devPath);
+                    DicConsole.WriteLine("READ CD-DA decoded sense:");
+                    DicConsole.Write("{0}", Decoders.SCSI.Sense.PrettifySense(senseBuffer));
+                    DicConsole.WriteLine("Press any key to continue...");
+                    System.Console.ReadKey();
+                    System.Console.Clear();
+                    DicConsole.WriteLine("Device: {0}", devPath);
+                    goto menu;
+                case 4:
+                    goto start;
+                case 5:
+                    goto parameters;
+                default:
+                    DicConsole.WriteLine("Incorrect option. Press any key to continue...");
+                    System.Console.ReadKey();
+                    System.Console.Clear();
+                    goto menu;
             }
         }
     }
