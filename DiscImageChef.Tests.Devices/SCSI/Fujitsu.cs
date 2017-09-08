8a61157c0c6167a9861a4e8089ac96109a678052
Natalia Portillo <claunia@claunia.com>
2017-09-08 20:51:08 +0100

    Added tests for Fujitsu vendor commands.

--- DiscImageChef.Tests.Devices/SCSI/Fujitsu.cs
+++ DiscImageChef.Tests.Devices/SCSI/Fujitsu.cs
@@ -66,11 +66,138 @@ namespace DiscImageChef.Tests.Devices.SCSI
                     case 0:
                         DicConsole.WriteLine("Returning to SCSI commands menu...");
                         return;
+                    case 1:
+                        Display(devPath, dev);
+                        continue;
                     default:
                         DicConsole.WriteLine("Incorrect option. Press any key to continue...");
                         System.Console.ReadKey();
                         continue;
                 }
+            }
+        }
+
+        static void Display(string devPath, Device dev)
+        {
+            bool flash = false;
+            FujitsuDisplayModes mode = FujitsuDisplayModes.Ready;
+            string firstHalf = "DIC TEST";
+            string secondHalf = "TEST DIC";
+            string strDev;
+            int item;
+
+        parameters:
+            while(true)
+            {
+                System.Console.Clear();
+                DicConsole.WriteLine("Device: {0}", devPath);
+                DicConsole.WriteLine("Parameters for DISPLAY command:");
+                DicConsole.WriteLine("Descriptor: {0}", flash);
+                DicConsole.WriteLine();
+                DicConsole.WriteLine("Choose what to do:");
+                DicConsole.WriteLine("1.- Change parameters.");
+                DicConsole.WriteLine("2.- Send command with these parameters.");
+                DicConsole.WriteLine("0.- Return to Fujitsu vendor commands menu.");
+
+                strDev = System.Console.ReadLine();
+                if(!int.TryParse(strDev, out item))
+                {
+                    DicConsole.WriteLine("Not a number. Press any key to continue...");
+                    System.Console.ReadKey();
+                    continue;
+                }
+
+                switch(item)
+                {
+                    case 0:
+                        DicConsole.WriteLine("Returning to Fujitsu vendor commands menu...");
+                        return;
+                    case 1:
+                        DicConsole.Write("Flash?: ");
+                        strDev = System.Console.ReadLine();
+                        if(!bool.TryParse(strDev, out flash))
+                        {
+                            DicConsole.WriteLine("Not a number. Press any key to continue...");
+                            flash = false;
+                            System.Console.ReadKey();
+                            continue;
+                        }
+                        DicConsole.WriteLine("Display mode");
+                        DicConsole.WriteLine("Available values: {0} {1} {2} {3} {4}", FujitsuDisplayModes.Cancel, FujitsuDisplayModes.Cart, FujitsuDisplayModes.Half, FujitsuDisplayModes.Idle, FujitsuDisplayModes.Ready);
+                        DicConsole.Write("Choose?: ");
+                        strDev = System.Console.ReadLine();
+                        if(!System.Enum.TryParse(strDev, true, out mode))
+                        {
+                            DicConsole.WriteLine("Not a correct display mode. Press any key to continue...");
+                            mode = FujitsuDisplayModes.Ready;
+                            System.Console.ReadKey();
+                            continue;
+                        }
+                        DicConsole.Write("First display half (will be cut to 7-bit ASCII, 8 chars?: ");
+                        firstHalf = System.Console.ReadLine();
+                        DicConsole.Write("Second display half (will be cut to 7-bit ASCII, 8 chars?: ");
+                        secondHalf = System.Console.ReadLine();
+                        break;
+                    case 2:
+                        goto start;
+                }
+            }
+
+        start:
+            System.Console.Clear();
+            bool sense = dev.FujitsuDisplay(out byte[] senseBuffer, flash, mode, firstHalf, secondHalf, dev.Timeout, out double duration);
+
+        menu:
+            DicConsole.WriteLine("Device: {0}", devPath);
+            DicConsole.WriteLine("Sending DISPLAY to the device:");
+            DicConsole.WriteLine("Command took {0} ms.", duration);
+            DicConsole.WriteLine("Sense is {0}.", sense);
+            DicConsole.WriteLine("Sense buffer is {0} bytes.", senseBuffer == null ? "null" : senseBuffer.Length.ToString());
+            DicConsole.WriteLine("Sense buffer is null or empty? {0}", ArrayHelpers.ArrayIsNullOrEmpty(senseBuffer));
+            DicConsole.WriteLine("DISPLAY decoded sense:");
+            DicConsole.Write("{0}", Decoders.SCSI.Sense.PrettifySense(senseBuffer));
+            DicConsole.WriteLine();
+            DicConsole.WriteLine("Choose what to do:");
+            DicConsole.WriteLine("1.- Print sense buffer.");
+            DicConsole.WriteLine("2.- Send command again.");
+            DicConsole.WriteLine("3.- Change parameters.");
+            DicConsole.WriteLine("0.- Return to Fujitsu vendor commands menu.");
+            DicConsole.Write("Choose: ");
+
+            strDev = System.Console.ReadLine();
+            if(!int.TryParse(strDev, out item))
+            {
+                DicConsole.WriteLine("Not a number. Press any key to continue...");
+                System.Console.ReadKey();
+                System.Console.Clear();
+                goto menu;
+            }
+
+            switch(item)
+            {
+                case 0:
+                    DicConsole.WriteLine("Returning to Fujitsu vendor commands menu...");
+                    return;
+                case 1:
+                    System.Console.Clear();
+                    DicConsole.WriteLine("Device: {0}", devPath);
+                    DicConsole.WriteLine("DISPLAY sense:");
+                    if(senseBuffer != null)
+                        PrintHex.PrintHexArray(senseBuffer, 64);
+                    DicConsole.WriteLine("Press any key to continue...");
+                    System.Console.ReadKey();
+                    System.Console.Clear();
+                    DicConsole.WriteLine("Device: {0}", devPath);
+                    goto menu;
+                case 2:
+                    goto start;
+                case 3:
+                    goto parameters;
+                default:
+                    DicConsole.WriteLine("Incorrect option. Press any key to continue...");
+                    System.Console.ReadKey();
+                    System.Console.Clear();
+                    goto menu;
             }
         }
     }
