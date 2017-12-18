f9f6ef3be2530b62b9ffc55fe1a6724ed68be1ae
Natalia Portillo <claunia@claunia.com>
2017-12-18 19:11:03 +0000

    Added file comments.

--- DiscImageChef.Device.Report/ata.h
+++ DiscImageChef.Device.Report/ata.h
@@ -1,6 +1,35 @@
-//
-// Created by claunia on 11/12/17.
-//
+/***************************************************************************
+The Disc Image Chef
+----------------------------------------------------------------------------
+
+Filename       : ata.h
+Version        : 4.0
+Author(s)      : Natalia Portillo
+
+Component      : DiscImageChef.Device.Report
+
+--[ Description ] ----------------------------------------------------------
+
+Contains ATA definitions.
+
+--[ License ] --------------------------------------------------------------
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+----------------------------------------------------------------------------
+Copyright (C) 2011-2018 Claunia.com
+****************************************************************************/
 
 #ifndef DISCIMAGECHEF_DEVICE_REPORT_ATA_H
 #define DISCIMAGECHEF_DEVICE_REPORT_ATA_H
--- DiscImageChef.Device.Report/scsi_mode.h
+++ DiscImageChef.Device.Report/scsi_mode.h
@@ -1,6 +1,35 @@
-//
-// Created by claunia on 16/12/17.
-//
+/***************************************************************************
+The Disc Image Chef
+----------------------------------------------------------------------------
+
+Filename       : scsi_mode.h
+Version        : 4.0
+Author(s)      : Natalia Portillo
+
+Component      : DiscImageChef.Device.Report
+
+--[ Description ] ----------------------------------------------------------
+
+Contains definitions for SCSI MODE PAGEs.
+
+--[ License ] --------------------------------------------------------------
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+----------------------------------------------------------------------------
+Copyright (C) 2011-2018 Claunia.com
+****************************************************************************/
 
 #ifndef DISCIMAGECHEF_DEVICE_REPORT_SCSI_MODE_H
 #define DISCIMAGECHEF_DEVICE_REPORT_SCSI_MODE_H
