b14320a44a0298a1d292a34b108550f942ea1f77
Natalia Portillo <claunia@claunia.com>
2018-04-11 08:13:49 +0100

    Update project schemas.

--- DiscImageChef.Filters/Properties/AssemblyInfo.cs
+++ DiscImageChef.Filters/Properties/AssemblyInfo.cs
@@ -35,12 +35,12 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("DiscImageChef.Filters")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("The Disc Image Chef")]
+[assembly: AssemblyDescription("The Disc Image Chef")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Claunia.com")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("© Claunia.com")]
+[assembly: AssemblyCopyright("Copyright © 2011-2018 Natalia Portillo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,7 +48,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.0.99.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
