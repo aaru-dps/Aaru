714ed8ef0f99806a88e573b41b4e9db8babaae90
Natalia Portillo <claunia@claunia.com>
2018-11-27 00:12:07 +0000

    Add constructors from device report version 2 to device and report models.

--- DiscImageChef.Database/Models/Device.cs
+++ DiscImageChef.Database/Models/Device.cs
@@ -5,6 +5,20 @@ namespace DiscImageChef.Database.Models
 {
     public class Device : DeviceReportV2
     {
+        public Device(DeviceReportV2 report)
+        {
+            ATA              = report.ATA;
+            ATAPI            = report.ATA;
+            CompactFlash     = report.CompactFlash;
+            FireWire         = report.FireWire;
+            LastSynchronized = DateTime.UtcNow;
+            MultiMediaCard   = report.MultiMediaCard;
+            PCMCIA           = report.PCMCIA;
+            SCSI             = report.SCSI;
+            SecureDigital    = report.SecureDigital;
+            USB              = report.USB;
+        }
+
         public DateTime LastSynchronized { get; set; }
     }
 }
--- DiscImageChef.Database/Models/Report.cs
+++ DiscImageChef.Database/Models/Report.cs
@@ -5,6 +5,21 @@ namespace DiscImageChef.Database.Models
 {
     public class Report : DeviceReportV2
     {
+        public Report(DeviceReportV2 report)
+        {
+            ATA            = report.ATA;
+            ATAPI          = report.ATA;
+            CompactFlash   = report.CompactFlash;
+            FireWire       = report.FireWire;
+            Created        = DateTime.UtcNow;
+            MultiMediaCard = report.MultiMediaCard;
+            PCMCIA         = report.PCMCIA;
+            SCSI           = report.SCSI;
+            SecureDigital  = report.SecureDigital;
+            USB            = report.USB;
+            Uploaded       = false;
+        }
+
         public DateTime Created  { get; set; }
         public bool     Uploaded { get; set; }
     }
