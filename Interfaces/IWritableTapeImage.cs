2605585cf9c17093463bca5f0bb346c8acbae653
Natalia Portillo <claunia@claunia.com>
2019-04-30 22:49:32 +0100

    Add interfaces for logical block addressable sequential (aka streaming tapes) images.

---
+++ Interfaces/IWritableTapeImage.cs
@@ -0,0 +1,59 @@
+// /***************************************************************************
+// The Disc Image Chef
+// ----------------------------------------------------------------------------
+//
+// Filename       : IWritableTapeImage.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : Disc image plugins.
+//
+// --[ Description ] ----------------------------------------------------------
+//
+//     Defines interface to be implemented by writable block addressable
+//     sequential tape image plugins.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a
+//     copy of this software and associated documentation files (the
+//     "Software"), to deal in the Software without restriction, including
+//     without limitation the rights to use, copy, modify, merge, publish,
+//     distribute, sublicense, and/or sell copies of the Software, and to
+//     permit persons to whom the Software is furnished to do so, subject to
+//     the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included
+//     in all copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2019 Natalia Portillo
+// ****************************************************************************/
+using DiscImageChef.CommonTypes.Structs;
+
+namespace DiscImageChef.CommonTypes.Interfaces
+{
+    public interface IWritableTapeImage : ITapeImage
+    {
+        /// <summary>
+        /// Registers a new file in the image
+        /// </summary>
+        /// <param name="file">Tape file descriptor</param>
+        /// <returns><c>true</c> if successful, <c>false</c> otherwise</returns>
+        bool AddFile(TapeFile file);
+
+        /// <summary>
+        /// Registers a new partition
+        /// </summary>
+        /// <param name="partition">Tape partition descriptor</param>
+        /// <returns><c>true</c> if successful, <c>false</c> otherwise</returns>
+        bool AddPartition(TapePartition partition);
+    }
+}
