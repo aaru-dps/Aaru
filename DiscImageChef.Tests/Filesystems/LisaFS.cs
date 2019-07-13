2b33412aad8d2a15db0fbf47c9c70b60bac7faca
Natalia Portillo <claunia@claunia.com>
2019-07-13 13:53:37 +0100

    Add more tests for Lisa filesystem.

--- DiscImageChef.Tests/Filesystems/LisaFS.cs
+++ DiscImageChef.Tests/Filesystems/LisaFS.cs
@@ -26,6 +26,7 @@
 // Copyright © 2011-2019 Natalia Portillo
 // ****************************************************************************/
 
+using System;
 using System.IO;
 using DiscImageChef.CommonTypes;
 using DiscImageChef.CommonTypes.Interfaces;
@@ -47,38 +48,45 @@ namespace DiscImageChef.Tests.Filesystems
             "three-folders-with-differently-named-docs.dc42.lz",
             "three-folders-with-differently-named-docs-root-alphabetical.dc42.lz",
             "three-folders-with-differently-named-docs-root-chronological.dc42.lz",
-            "three-folders-with-identically-named-docs.dc42.lz"
+            "three-folders-with-identically-named-docs.dc42.lz", "lisafs1.dc42.lz", "lisafs2.dc42.lz",
+            "lisafs3.dc42.lz", "lisafs3_with_desktop.dc42.lz"
         };
 
         readonly MediaType[] mediatypes =
         {
             MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS,
             MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS,
-            MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS
+            MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS,
+            MediaType.AppleFileWare, MediaType.AppleSonySS, MediaType.AppleSonySS, MediaType.AppleSonySS
         };
 
-        readonly ulong[] sectors = {800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800};
+        readonly ulong[] sectors = {800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 1702, 800, 800, 800};
 
-        readonly uint[] sectorsize = {512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512};
+        readonly uint[] sectorsize = {512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512};
 
-        readonly long[] clusters = {800, 800, 792, 800, 800, 800, 800, 800, 800, 800, 800, 800};
+        readonly long[] clusters = {800, 800, 792, 800, 800, 800, 800, 800, 800, 800, 800, 800, 1684, 792, 800, 800};
 
-        readonly int[] clustersize = {512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512};
+        readonly int[] clustersize = {512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512};
 
         readonly string[] volumename =
         {
             "166Files", "222Files", "AOS  4:59 pm 10/02/87", "AOS 3.0", "AOS 3.0", "AOS 3.0", "AOS 3.0", "AOS 3.0",
-            "AOS 3.0", "AOS 3.0", "AOS 3.0", "AOS 3.0"
+            "AOS 3.0", "AOS 3.0", "AOS 3.0", "AOS 3.0", "AOS 4:15 pm 5/06/1983", "Office System 1 2.0",
+            "Office System 1 3.0", "AOS 3.0"
         };
 
         readonly string[] volumeserial =
         {
             "A23703A202010663", "A23703A201010663", "A32D261301010663", "A22CB48D01010663", "A22CC3A702010663",
             "A22CB48D14010663", "A22CB48D14010663", "A22CB48D01010663", "A22CB48D01010663", "A22CB48D01010663",
-            "A22CB48D01010663", "A22CB48D01010663"
+            "A22CB48D01010663", "A22CB48D01010663", "9924151E190001E1", "9497F10016010D10", "9CF9CF89070100A8",
+            "A4FE1A191F011652"
         };
 
-        readonly string[] oemid = {null, null, null, null, null, null, null, null, null, null, null, null};
+        readonly string[] oemid =
+        {
+            null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null
+        };
 
         [Test]
         public void Test()
@@ -109,6 +117,12 @@ namespace DiscImageChef.Tests.Filesystems
                 Assert.AreEqual(volumeserial[i], fs.XmlFsType.VolumeSerial,     testfiles[i]);
                 Assert.AreEqual(oemid[i],        fs.XmlFsType.SystemIdentifier, testfiles[i]);
             }
+        }
+
+        [Test]
+        public void TestContents()
+        {
+            throw new NotImplementedException();
         }
     }
 }
