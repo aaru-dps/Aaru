7fd5b16b1d101a24f773c58f777a28ecc4d5e99f
Natalia Portillo <claunia@claunia.com>
2019-10-13 20:54:10 +0100

    Add NOP packet.

--- DiscImageChef.Devices/Remote/Enums.cs
+++ DiscImageChef.Devices/Remote/Enums.cs
@@ -1,7 +1,8 @@
 namespace DiscImageChef.Devices.Remote
 {
-    public enum DicPacketType : byte
+    public enum DicPacketType : sbyte
     {
+        Nop = -1,
         Hello = 1,
         CommandListDevices = 2,
         ResponseListDevices = 3
