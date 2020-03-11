c3783ac4c20468bbf93c77bcfe3eda1166eea7ff
Natalia Portillo <claunia@claunia.com>
2020-03-11 21:56:54 +0000

    Complete file headers.

--- CRC16IBMContext.cs
+++ CRC16IBMContext.cs
@@ -1,3 +1,35 @@
+// /***************************************************************************
+// Aaru Data Preservation Suite
+// ----------------------------------------------------------------------------
+//
+// Filename       : CRC16IBMContext.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : Checksums.
+//
+// --[ Description ] ----------------------------------------------------------
+//
+//     Implements a CRC16 algorithm with the IBM polynomial.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This library is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as
+//     published by the Free Software Foundation; either version 2.1 of the
+//     License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful, but
+//     WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2020 Natalia Portillo
+// ****************************************************************************/
+
 namespace Aaru.Checksums
 {
     public class CRC16IBMContext : Crc16Context
