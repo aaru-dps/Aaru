e484671504964035ab7532c42b257a13c403d614
Natalia Portillo <claunia@claunia.com>
2020-04-09 18:18:56 +0100

    Add menu to MainWindow.

--- Aaru.Gui/ViewModels/MainWindowViewModel.cs
+++ Aaru.Gui/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,15 @@
-﻿namespace Aaru.Gui.ViewModels
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace Aaru.Gui.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Aaru!";
+
+        public bool NativeMenuNotSupported => !NativeMenu.GetIsNativeMenuExported((Application.Current.ApplicationLifetime as
+                                                                                       IClassicDesktopStyleApplicationLifetime)?.MainWindow);
     }
 }
