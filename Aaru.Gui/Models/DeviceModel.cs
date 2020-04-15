b81d7180b51262ee99498d0da31f89bbbed64b09
Natalia Portillo <claunia@claunia.com>
2020-04-15 01:54:07 +0100

    Add model for media.

--- Aaru.Gui/Models/DeviceModel.cs
+++ Aaru.Gui/Models/DeviceModel.cs
@@ -1,11 +1,16 @@
+using System.Collections.ObjectModel;
 using Avalonia.Media.Imaging;
 
 namespace Aaru.Gui.Models
 {
     public class DeviceModel
     {
+        public DeviceModel() => Media = new ObservableCollection<MediaModel>();
+
         public Bitmap Icon { get; set; }
         public string Name { get; set; }
         public string Path { get; set; }
+
+        public ObservableCollection<MediaModel> Media { get; }
     }
 }
---
+++ Aaru.Gui/Models/MediaModel.cs
@@ -0,0 +1,11 @@
+using Avalonia.Media.Imaging;
+
+namespace Aaru.Gui.Models
+{
+    public class MediaModel
+    {
+        public Bitmap Icon       { get; set; }
+        public string Name       { get; set; }
+        public string DevicePath { get; set; }
+    }
+}
