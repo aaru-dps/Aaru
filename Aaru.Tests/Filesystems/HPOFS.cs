950c3d7c973cef88f98ee6f245bd3db4334e4d21
Natalia Portillo <claunia@claunia.com>
2020-07-25 01:58:50 +0100

    Fix copyright dates.

--- Aaru.Tests/Filesystems/HPOFS.cs
+++ Aaru.Tests/Filesystems/HPOFS.cs
@@ -23,7 +23,7 @@
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // ----------------------------------------------------------------------------
-// Copyright © 2011-2019 Natalia Portillo
+// Copyright © 2011-2020 Natalia Portillo
 // ****************************************************************************/
 
 using System.IO;
