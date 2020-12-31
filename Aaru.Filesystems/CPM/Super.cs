472a1d94d87e94b566fb2b4f6eff72311f1e1e14
Natalia Portillo <claunia@claunia.com>
2020-12-31 23:08:23 +0000

    Update copyright year.

--- Aaru.Filesystems/CPM/Super.cs
+++ Aaru.Filesystems/CPM/Super.cs
@@ -29,7 +29,7 @@
 //     License along with this library; if not, see <http://www.gnu.org/licenses/>.
 //
 // ----------------------------------------------------------------------------
-// Copyright © 2011-2020 Natalia Portillo
+// Copyright © 2011-2021 Natalia Portillo
 // ****************************************************************************/
 
 using System;
