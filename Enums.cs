a5947b590b6a137d3dbb6dada3e6d093857072d0
Rebecca Wallander <sakcheen@gmail.com>
2021-01-13 21:09:13 +0100

    Add CSS decryption

---
+++ Enums.cs
@@ -0,0 +1,12 @@
+namespace Aaru.Decryption
+{
+    public enum DvdCssKeyClass : byte
+    {
+        DvdCssCppmOrCprm = 0, RewritableSecurityServicesA = 1
+    }
+
+    public enum DvdCssKeyType
+    {
+        Key1 = 0, Key2 = 1, BusKey = 2
+    }
+}
