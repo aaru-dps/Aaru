ad087df1a334a3cee45eab7069facb3da0afbfa2
Natalia Portillo <claunia@claunia.com>
2021-02-22 04:49:05 +0000

    Add new 2MG test image.

--- Aaru.Tests/Images/2MG.cs
+++ Aaru.Tests/Images/2MG.cs
@@ -46,19 +46,19 @@ namespace Aaru.Tests.Images
             "pascal800_do.2mg.lz", "pascal800_p.2mg.lz", "pascal_dic.2mg.lz", "pascal_do.2mg.lz", "pascal_nib.2mg.lz",
             "pascal_po.2mg.lz", "prodos1440.2mg.lz", "prodos1440_po.2mg.lz", "prodos5mb.2mg.lz", "prodos5m_dic.2mg.lz",
             "prodos800_dic.2mg.lz", "prodos800_do.2mg.lz", "prodos800_po.2mg.lz", "prodos_dic.2mg.lz",
-            "prodos_do.2mg.lz", "prodos_nib.2mg.lz", "prodos_po.2mg.lz"
+            "prodos_do.2mg.lz", "prodos_nib.2mg.lz", "prodos_po.2mg.lz", "prodos.2mg.lz"
         };
 
         readonly ulong[] _sectors =
         {
             560, 455, 455, 560, 560, 560, 560, 560, 560, 2880, 1600, 1600, 1600, 1600, 1600, 1600, 1600, 560, 560, 560,
-            560, 2880, 2880, 10240, 10240, 1600, 1600, 1600, 560, 560, 560, 560
+            560, 2880, 2880, 10240, 10240, 1600, 1600, 1600, 560, 560, 560, 560, 560
         };
 
         readonly uint[] _sectorSize =
         {
             256, 256, 256, 256, 256, 256, 256, 256, 256, 512, 512, 512, 512, 512, 512, 512, 512, 256, 256, 256, 256,
-            512, 512, 512, 512, 512, 512, 512, 256, 256, 256, 256
+            512, 512, 512, 512, 512, 512, 512, 256, 256, 256, 256, 256
         };
 
         readonly MediaType[] _mediaTypes =
@@ -69,7 +69,7 @@ namespace Aaru.Tests.Images
             MediaType.AppleSonyDS, MediaType.AppleSonyDS, MediaType.AppleSonyDS, MediaType.Apple33SS,
             MediaType.Apple33SS, MediaType.Apple33SS, MediaType.Apple33SS, MediaType.DOS_35_HD, MediaType.DOS_35_HD,
             MediaType.Unknown, MediaType.Unknown, MediaType.AppleSonyDS, MediaType.AppleSonyDS, MediaType.AppleSonyDS,
-            MediaType.Apple33SS, MediaType.Apple33SS, MediaType.Apple33SS, MediaType.Apple33SS
+            MediaType.Apple33SS, MediaType.Apple33SS, MediaType.Apple33SS, MediaType.Apple33SS, MediaType.Apple33SS
         };
 
         readonly string[] _md5S =
@@ -84,7 +84,7 @@ namespace Aaru.Tests.Images
             "eb9b60c78b30d2b6541ed0781944b6da", "1fe841b418ede51133878641e01544b5", "b156441e159a625ee00a0659dfb6e2f8",
             "b156441e159a625ee00a0659dfb6e2f8", "fcf747bd356b48d442ff74adb8f3516b", "fcf747bd356b48d442ff74adb8f3516b",
             "fcf747bd356b48d442ff74adb8f3516b", "11ef56c80c94347d2e3f921d5c36c8de", "11ef56c80c94347d2e3f921d5c36c8de",
-            "11ef56c80c94347d2e3f921d5c36c8de", "11ef56c80c94347d2e3f921d5c36c8de"
+            "11ef56c80c94347d2e3f921d5c36c8de", "11ef56c80c94347d2e3f921d5c36c8de", "6f692a8fadfaa243d9f2d8d41f0e4cad"
         };
 
         [Test]
