f36cb789589d3696d138b6fcc3853dbcb2e0f0ef
Natalia Portillo <claunia@claunia.com>
2021-02-22 13:31:52 +0000

    Fix media type for unknown tape.

--- MediaType.cs
+++ MediaType.cs
@@ -68,8 +68,8 @@ namespace Aaru.CommonTypes
         Zone_HDD = 4,
         /// <summary>USB flash drives</summary>
         FlashDrive = 5,
-        /// <summary>USB flash drives</summary>
-        UnknownTape = 4,
+        /// <summary>Unknown data tape</summary>
+        UnknownTape = 6,
         #endregion Generics, types 0 to 9
 
         #region Somewhat standard Compact Disc formats, types 10 to 39
