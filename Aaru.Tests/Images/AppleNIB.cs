04aed8a2e578b8bd1ec0452c518a0712f5d65194
Natalia Portillo <claunia@claunia.com>
2021-02-26 16:14:58 +0000

    Add unit tests for Apple nibble images.

---
+++ Aaru.Tests/Images/AppleNIB.cs
@@ -0,0 +1,191 @@
+﻿// /***************************************************************************
+// Aaru Data Preservation Suite
+// ----------------------------------------------------------------------------
+//
+// Filename       : AppleNIB.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : Aaru unit testing.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2011-2021 Natalia Portillo
+// ****************************************************************************/
+
+using System;
+using System.IO;
+using Aaru.Checksums;
+using Aaru.CommonTypes;
+using Aaru.Filters;
+using FluentAssertions.Execution;
+using NUnit.Framework;
+
+namespace Aaru.Tests.Images
+{
+    [TestFixture]
+    public class AppleNib
+    {
+        readonly string[] _testFiles =
+        {
+            "dos32.nib.lz", "dos33.nib.lz", "pascal.nib.lz", "prodos.nib.lz"
+        };
+
+        readonly ulong[] _sectors =
+        {
+            // dos32.nib.lz
+            455,
+
+            // dos33.nib.lz
+            560,
+
+            // pascal.nib.lz
+            560,
+
+            // prodos.nib.lz
+            560
+        };
+
+        readonly uint[] _sectorSize =
+        {
+            // dos32.nib.lz
+            256,
+
+            // dos33.nib.lz
+            256,
+
+            // pascal.nib.lz
+            256,
+
+            // prodos.nib.lz
+            256
+        };
+
+        readonly MediaType[] _mediaTypes =
+        {
+            // dos32.nib.lz
+            MediaType.Apple32SS,
+
+            // dos33.nib.lz
+            MediaType.Apple33SS,
+
+            // pascal.nib.lz
+            MediaType.Apple33SS,
+
+            // prodos.nib.lz
+            MediaType.Apple33SS
+        };
+
+        readonly string[] _md5S =
+        {
+            // dos32.nib.lz
+            "76f8fe4c5bc1976f99641ad7cdf53109",
+
+            // dos33.nib.lz
+            "0ffcbd4180306192726926b43755db2f",
+
+            // pascal.nib.lz
+            "4c4926103a32ac15f7e430ec3ced4be5",
+
+            // prodos.nib.lz
+            "11ef56c80c94347d2e3f921d5c36c8de"
+        };
+
+        readonly string _dataFolder = Path.Combine(Consts.TEST_FILES_ROOT, "Media image formats", "Nibbles");
+
+        [Test]
+        public void Info()
+        {
+            Environment.CurrentDirectory = _dataFolder;
+
+            Assert.Multiple(() =>
+            {
+                for(int i = 0; i < _testFiles.Length; i++)
+                {
+                    var filter = new LZip();
+                    filter.Open(_testFiles[i]);
+
+                    var  image  = new DiscImages.AppleNib();
+                    bool opened = image.Open(filter);
+
+                    Assert.AreEqual(true, opened, $"Open: {_testFiles[i]}");
+
+                    if(!opened)
+                        continue;
+
+                    using(new AssertionScope())
+                    {
+                        Assert.Multiple(() =>
+                        {
+                            Assert.AreEqual(_sectors[i], image.Info.Sectors, $"Sectors: {_testFiles[i]}");
+                            Assert.AreEqual(_sectorSize[i], image.Info.SectorSize, $"Sector size: {_testFiles[i]}");
+                            Assert.AreEqual(_mediaTypes[i], image.Info.MediaType, $"Media type: {_testFiles[i]}");
+                        });
+                    }
+                }
+            });
+        }
+
+        // How many sectors to read at once
+        const uint _sectorsToRead = 256;
+
+        [Test]
+        public void Hashes()
+        {
+            Environment.CurrentDirectory = _dataFolder;
+
+            Assert.Multiple(() =>
+            {
+                for(int i = 0; i < _testFiles.Length; i++)
+                {
+                    var filter = new LZip();
+                    filter.Open(_testFiles[i]);
+
+                    var   image       = new DiscImages.AppleNib();
+                    bool  opened      = image.Open(filter);
+                    ulong doneSectors = 0;
+
+                    Assert.AreEqual(true, opened, $"Open: {_testFiles[i]}");
+
+                    if(!opened)
+                        continue;
+
+                    var ctx = new Md5Context();
+
+                    while(doneSectors < image.Info.Sectors)
+                    {
+                        byte[] sector;
+
+                        if(image.Info.Sectors - doneSectors >= _sectorsToRead)
+                        {
+                            sector      =  image.ReadSectors(doneSectors, _sectorsToRead);
+                            doneSectors += _sectorsToRead;
+                        }
+                        else
+                        {
+                            sector      =  image.ReadSectors(doneSectors, (uint)(image.Info.Sectors - doneSectors));
+                            doneSectors += image.Info.Sectors - doneSectors;
+                        }
+
+                        ctx.Update(sector);
+                    }
+
+                    Assert.AreEqual(_md5S[i], ctx.End(), $"Hash: {_testFiles[i]}");
+                }
+            });
+        }
+    }
+}
