65b74e5dbecb5c529ae42d0949d286830cc59e29
Natalia Portillo <claunia@claunia.com>
2021-06-01 12:44:25 +0100

    Fix wrong JSON library used.

--- Structs/Filesystems.cs
+++ Structs/Filesystems.cs
@@ -40,7 +40,7 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Aaru.CommonTypes.Structs
 {
