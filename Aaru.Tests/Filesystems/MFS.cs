3d36eecceec5104d79e9aa3da02e39d9811d8ec8
Natalia Portillo <claunia@claunia.com>
2021-06-02 03:15:44 +0100

    Implement ReadOnlyFilesystemTest in Macintosh File System tests.

--- Aaru.Tests/Filesystems/MFS.cs
+++ Aaru.Tests/Filesystems/MFS.cs
@@ -35,7 +35,7 @@ using NUnit.Framework;
 namespace Aaru.Tests.Filesystems
 {
     [TestFixture]
-    public class Mfs : FilesystemTest
+    public class Mfs : ReadOnlyFilesystemTest
     {
         public Mfs() : base("MFS") {}
 
