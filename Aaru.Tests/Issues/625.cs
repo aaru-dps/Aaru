bcc413514667860045770bf0b8b3879c6e680a0f
Natalia Portillo <claunia@claunia.com>
2021-08-03 04:36:38 +0100

    Add unit test for #625

---
+++ Aaru.Tests/Issues/625.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using Aaru.CommonTypes;
+using Aaru.CommonTypes.Interfaces;
+using Aaru.DiscImages;
+using Aaru.Tests.WritableImages;
+
+namespace Aaru.Tests.Issues
+{
+    public class _625 : WritableOpticalMediaImageTest
+    {
+        public override string DataFolder => Path.Combine(Consts.TEST_FILES_ROOT, "Issues", "Pending", "issue625");
+        public override IMediaImage InputPlugin => new Cdrdao();
+        public override IWritableImage OutputPlugin => new CloneCd();
+        public override string OutputExtension => "mds";
+        public override OpticalImageTestExpected[] Tests => new[]
+        {
+            new OpticalImageTestExpected
+            {
+                TestFile      = "alice.toc",
+                MediaType     = MediaType.CDROM,
+                Sectors       = 255,
+                SectorSize    = 2048,
+                LongMD5       = "1bea7f781be0fb3b878de96e965c53a0",
+                SubchannelMD5 = "01fef9f42fe53e6256ba713ad237dc8c",
+                Tracks = new[]
+                {
+                    new TrackInfoTestExpected
+                    {
+                        Session = 1,
+                        Start   = 0,
+                        End     = 254,
+                        Pregap  = 150,
+                        Flags   = 4
+                    }
+                }
+            }
+        };
+    }
+}
