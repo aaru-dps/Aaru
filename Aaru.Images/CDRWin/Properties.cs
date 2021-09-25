bfd5b4f390c83c8b8d9a1a09bb3cf631a32ab643
Rebecca Wallander <sakcheen@gmail.com>
2021-09-25 20:49:57 +0100

    Add pregap to second session in redump gdrom

--- Aaru.Images/CDRWin/Properties.cs
+++ Aaru.Images/CDRWin/Properties.cs
@@ -120,14 +120,17 @@ namespace Aaru.DiscImages
                     foreach((ushort index, int position) in cdrTrack.Indexes)
                         aaruTrack.Indexes[index] = position;
 
-                    if(_discImage.IsRedumpGigadisc &&
-                       cdrTrack.Session    == 2    &&
-                       previousStartSector < gdRomSession2Offset)
-                        aaruTrack.StartSector = gdRomSession2Offset;
-
                     previousTrackFile = cdrTrack.TrackFile.DataFilter.Filename;
 
                     aaruTrack.EndSector = aaruTrack.StartSector + cdrTrack.Sectors - 1;
+
+                    if(_discImage.IsRedumpGigadisc &&
+                       cdrTrack.Session    == 2    &&
+                       previousStartSector < gdRomSession2Offset)
+                    {
+                        aaruTrack.StartSector = (ulong)cdrTrack.Indexes[0];
+                        aaruTrack.EndSector   = gdRomSession2Offset + cdrTrack.Sectors - (uint)cdrTrack.Pregap - 1;
+                    }
 
                     if(cdrTrack.TrackType == CDRWIN_TRACK_TYPE_CDG)
                     {
