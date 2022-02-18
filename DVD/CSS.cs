3d303623c22b3b27e5c9d134958ad7a2dfff7663
Natalia Portillo <claunia@claunia.com>
2022-02-18 10:02:34 +0000

    Update copyright year.

--- DVD/CSS.cs
+++ DVD/CSS.cs
@@ -31,7 +31,7 @@
 //     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 // ----------------------------------------------------------------------------
-// Copyright © 2020-2021 Rebecca Wallander
+// Copyright © 2020-2022 Rebecca Wallander
 // ****************************************************************************/
 
 using System;
