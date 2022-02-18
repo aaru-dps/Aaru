7098c95929a0fddb8ed44be8db2241c63266dc2c
Natalia Portillo <claunia@claunia.com>
2022-02-18 10:02:42 +0000

    Update copyright year.

--- AaruConsole.cs
+++ AaruConsole.cs
@@ -27,7 +27,7 @@
 //     License along with this library; if not, see <http://www.gnu.org/licenses/>.
 //
 // ----------------------------------------------------------------------------
-// Copyright © 2011-2021 Natalia Portillo
+// Copyright © 2011-2022 Natalia Portillo
 // ****************************************************************************/
 
 namespace Aaru.Console
