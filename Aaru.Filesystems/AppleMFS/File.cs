0fc479a5299637b37f425607b315701367dab4ff
Natalia Portillo <claunia@claunia.com>
2022-12-19 10:57:37 +0000

    [Macintosh filesystem] Fix calculation of file sizes.

--- Aaru.Filesystems/AppleMFS/File.cs
+++ Aaru.Filesystems/AppleMFS/File.cs
@@ -349,12 +349,12 @@ public sealed partial class AppleMFS
         stat = new FileEntryInfo
         {
             Attributes    = attr,
-            Blocks        = entry.flLgLen / _volMdb.drAlBlkSiz,
+            Blocks        = entry.flPyLen / _volMdb.drAlBlkSiz,
             BlockSize     = _volMdb.drAlBlkSiz,
             CreationTime  = DateHandlers.MacToDateTime(entry.flCrDat),
             Inode         = entry.flFlNum,
             LastWriteTime = DateHandlers.MacToDateTime(entry.flMdDat),
-            Length        = entry.flPyLen,
+            Length        = entry.flLgLen,
             Links         = 1
         };
 
