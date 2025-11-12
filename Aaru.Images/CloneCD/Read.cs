e57c8a979beab7512ed5509bf62004c5a26c773d
HeroponRikiBestest <jojloj3@gmail.com>
2025-11-12 09:43:10 -0500

    Bulk add unimplemented API

--- Aaru.Images/CloneCD/Read.cs
+++ Aaru.Images/CloneCD/Read.cs
@@ -909,6 +909,23 @@ public sealed partial class CloneCd
     public ErrorNumber ReadSectorTag(ulong sectorAddress, bool negative, SectorTagType tag, out byte[] buffer) =>
         ReadSectorsTag(sectorAddress, negative, 1, tag, out buffer);
 
+    public ErrorNumber ReadDPM(out uint dpmStartSector, out uint dpmResolution, out uint numberOfDpmEntries, out ulong[] dpm)
+    {
+        dpmStartSector = 0;
+        dpmResolution = 0;
+        numberOfDpmEntries = 0;
+        dpm = null;
+
+        return ErrorNumber.NotSupported;
+    }
+
+    public ErrorNumber ReadSectorDPM(ulong sectorAddress, out ulong? dpm)
+    {
+        dpm = null;
+
+        return ErrorNumber.NotSupported;
+    }
+
     /// <inheritdoc />
     public ErrorNumber ReadSector(ulong sectorAddress, uint track, out byte[] buffer, out SectorStatus sectorStatus)
     {
