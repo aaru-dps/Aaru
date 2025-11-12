4fcc432cb4c0639600ab66b63a3382d2559e8d88
HeroponRikiBestest <jojloj3@gmail.com>
2025-11-12 09:43:10 -0500

    Bulk add unimplemented API

--- Aaru.Images/CHD/Read.cs
+++ Aaru.Images/CHD/Read.cs
@@ -1989,6 +1989,23 @@ public sealed partial class Chd
     public List<Track> GetSessionTracks(ushort session) =>
         _isHdd ? null : _tracks.Values.Where(track => track.Session == session).ToList();
 
+    public ErrorNumber ReadDPM(out uint dpmStartSector, out uint dpmResolution, out uint numberOfDpmEntries, out ulong[] dpm)
+    {
+        dpmStartSector = 0;
+        dpmResolution = 0;
+        numberOfDpmEntries = 0;
+        dpm = null;
+
+        return ErrorNumber.NotSupported;
+    }
+
+    public ErrorNumber ReadSectorDPM(ulong sectorAddress, out ulong? dpm)
+    {
+        dpm = null;
+
+        return ErrorNumber.NotSupported;
+    }
+
     /// <inheritdoc />
     public ErrorNumber ReadSector(ulong sectorAddress, uint track, out byte[] buffer, out SectorStatus sectorStatus)
     {
