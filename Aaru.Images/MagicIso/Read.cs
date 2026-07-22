89c353747d0eefe4b37636b236d40543f9a03260
HeroponRikiBestest <jojloj3@gmail.com>
2026-07-22 08:32:10 -0700

    Add needed implementations of ReadDPM and ReadSectorDPM to newly added Read.cs entries since last PR commit.

--- Aaru.Images/MagicIso/Read.cs
+++ Aaru.Images/MagicIso/Read.cs
@@ -990,6 +990,25 @@ public sealed partial class MagicIso
     }
 
     /// <inheritdoc />
+    public ErrorNumber ReadDPM(out uint dpmStartSector, out uint dpmResolution, out uint numberOfDpmEntries, out ulong[] dpm)
+    {
+        dpmStartSector     = 0;
+        dpmResolution      = 0;
+        numberOfDpmEntries = 0;
+        dpm                = null;
+
+        return ErrorNumber.NotSupported;
+    }
+
+    /// <inheritdoc />
+    public ErrorNumber ReadSectorDPM(ulong sectorAddress, out ulong? dpm)
+    {
+        dpm = null;
+
+        return ErrorNumber.NotSupported;
+    }
+
+    /// <inheritdoc />
     public ErrorNumber ReadSector(ulong sectorAddress, uint track, out byte[] buffer, out SectorStatus sectorStatus) =>
         ReadSectors(sectorAddress, 1, track, out buffer, out SectorStatus[] s) == ErrorNumber.NoError
             ? SetStatusAndReturn(out sectorStatus, s)
--- Aaru.Images/UltraISO/Read.cs
+++ Aaru.Images/UltraISO/Read.cs
@@ -193,6 +193,25 @@ public sealed partial class UltraISO
 #region IOpticalMediaImage Members
 
     /// <inheritdoc />
+    public ErrorNumber ReadDPM(out uint dpmStartSector, out uint dpmResolution, out uint numberOfDpmEntries, out ulong[] dpm)
+    {
+        dpmStartSector     = 0;
+        dpmResolution      = 0;
+        numberOfDpmEntries = 0;
+        dpm                = null;
+
+        return ErrorNumber.NotSupported;
+    }
+
+    /// <inheritdoc />
+    public ErrorNumber ReadSectorDPM(ulong sectorAddress, out ulong? dpm)
+    {
+        dpm = null;
+
+        return ErrorNumber.NotSupported;
+    }
+
+    /// <inheritdoc />
     public ErrorNumber ReadSector(ulong sectorAddress, uint track, out byte[] buffer, out SectorStatus sectorStatus)
     {
         buffer       = null;
