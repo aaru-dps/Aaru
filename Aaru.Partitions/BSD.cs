63be9acdce3dc5957d7703521c70ce19bd5f9a8b
Natalia Portillo <claunia@claunia.com>
2026-08-08 17:14:33 +0100

    [Partitions] Widen BSD disklabel byte arithmetic to avoid 32-bit wrap past 4 GiB.

--- Aaru.Partitions/BSD.cs
+++ Aaru.Partitions/BSD.cs
@@ -47,18 +47,18 @@ namespace Aaru.Partitions;
 
 /// <inheritdoc />
 /// <summary>Implements decoding of BSD disklabels</summary>
-[SuppressMessage("ReSharper", "UnusedMember.Local")]
+[SuppressMessage("ReSharper", "UnusedMember.Local", Justification = "Members document the full on-disk layout")]
 public sealed partial class BSD : IPartition
 {
-    const uint DISK_MAGIC = 0x82564557;
-    const uint DISK_CIGAM = 0x57455682;
+    const    uint    DISK_MAGIC      = 0x82564557;
+    const    uint    DISK_CIGAM      = 0x57455682;
     /// <summary>Maximum size of a disklabel with 22 partitions</summary>
-    const uint MAX_LABEL_SIZE = 500;
-    const string MODULE_NAME = "BSD disklabel plugin";
+    const    uint    MAX_LABEL_SIZE  = 500;
+    const    string  MODULE_NAME     = "BSD disklabel plugin";
     /// <summary>Known sector locations for BSD disklabel</summary>
     readonly ulong[] _labelLocations = [0, 1, 2, 9];
     /// <summary>Known byte offsets for BSD disklabel</summary>
-    readonly uint[] _labelOffsets = [0, 9, 64, 128, 516];
+    readonly uint[]  _labelOffsets   = [0, 9, 64, 128, 516];
 
 #region IPartition Members
 
@@ -175,10 +175,10 @@ public sealed partial class BSD : IPartition
 
             var part = new Partition
             {
-                Start    = dl.d_partitions[i].p_offset * dl.d_secsize / imagePlugin.Info.SectorSize,
-                Offset   = dl.d_partitions[i].p_offset                * dl.d_secsize,
-                Length   = dl.d_partitions[i].p_size * dl.d_secsize   / imagePlugin.Info.SectorSize,
-                Size     = dl.d_partitions[i].p_size                  * dl.d_secsize,
+                Start    = (ulong)dl.d_partitions[i].p_offset * dl.d_secsize / imagePlugin.Info.SectorSize,
+                Offset   = (ulong)dl.d_partitions[i].p_offset                * dl.d_secsize,
+                Length   = (ulong)dl.d_partitions[i].p_size * dl.d_secsize   / imagePlugin.Info.SectorSize,
+                Size     = (ulong)dl.d_partitions[i].p_size                  * dl.d_secsize,
                 Type     = FSTypeToString(dl.d_partitions[i].p_fstype),
                 Sequence = counter,
                 Scheme   = Name
@@ -249,15 +249,15 @@ public sealed partial class BSD : IPartition
     partial struct BSDPartition
     {
         /// <summary>Sectors in partition</summary>
-        public uint p_size;
+        public uint   p_size;
         /// <summary>Starting sector</summary>
-        public uint p_offset;
+        public uint   p_offset;
         /// <summary>Fragment size</summary>
-        public uint p_fsize;
+        public uint   p_fsize;
         /// <summary>Filesystem type, <see cref="fsType" /></summary>
         public fsType p_fstype;
         /// <summary>Fragment size</summary>
-        public byte p_frag;
+        public byte   p_frag;
         /// <summary>Cylinder per group</summary>
         public ushort p_cpg;
     }
@@ -267,22 +267,22 @@ public sealed partial class BSD : IPartition
 #region Nested type: dFlags
 
     /// <summary>Drive flags</summary>
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Names match the original BSD headers")]
     [Flags]
     enum dFlags : uint
     {
         /// <summary>Removable media</summary>
-        Removable = 0x01,
+        Removable        = 0x01,
         /// <summary>Drive supports ECC</summary>
-        ECC = 0x02,
+        ECC              = 0x02,
         /// <summary>Drive supports bad sector forwarding</summary>
         BadSectorForward = 0x04,
         /// <summary>Disk emulator</summary>
-        RAMDisk = 0x08,
+        RAMDisk          = 0x08,
         /// <summary>Can do back to back transfer</summary>
-        Chain = 0x10,
+        Chain            = 0x10,
         /// <summary>Dynamic geometry device</summary>
-        DynamicGeometry = 0x20
+        DynamicGeometry  = 0x20
     }
 
 #endregion
@@ -296,11 +296,11 @@ public sealed partial class BSD : IPartition
         /// <summary>
         ///     <see cref="BSD.DISK_MAGIC" />
         /// </summary>
-        public uint d_magic;
+        public uint   d_magic;
         /// <summary>
         ///     <see cref="dType" />
         /// </summary>
-        public dType d_type;
+        public dType  d_type;
         /// <summary>Disk subtype</summary>
         public ushort d_subtype;
         /// <summary>Type name</summary>
@@ -310,23 +310,23 @@ public sealed partial class BSD : IPartition
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
         public byte[] d_packname;
         /// <summary>Bytes per sector</summary>
-        public uint d_secsize;
+        public uint   d_secsize;
         /// <summary>Sectors per track</summary>
-        public uint d_nsectors;
+        public uint   d_nsectors;
         /// <summary>Tracks per cylinder</summary>
-        public uint d_ntracks;
+        public uint   d_ntracks;
         /// <summary>Cylinders per unit</summary>
-        public uint d_ncylinders;
+        public uint   d_ncylinders;
         /// <summary>Sectors per cylinder</summary>
-        public uint d_secpercyl;
+        public uint   d_secpercyl;
         /// <summary>Sectors per unit</summary>
-        public uint d_secperunit;
+        public uint   d_secperunit;
         /// <summary>Spare sectors per track</summary>
         public ushort d_sparespertrack;
         /// <summary>Spare sectors per cylinder</summary>
         public ushort d_sparespercyl;
         /// <summary>Alternate cylinders</summary>
-        public uint d_acylinders;
+        public uint   d_acylinders;
         /// <summary>Rotational speed</summary>
         public ushort d_rpm;
         /// <summary>Hardware sector interleave</summary>
@@ -336,9 +336,9 @@ public sealed partial class BSD : IPartition
         /// <summary>Sector 0 sker per cylinder</summary>
         public ushort d_cylskeew;
         /// <summary>Head switch time in microseconds</summary>
-        public uint d_headswitch;
+        public uint   d_headswitch;
         /// <summary>Track to track seek in microseconds</summary>
-        public uint d_trkseek;
+        public uint   d_trkseek;
         /// <summary>
         ///     <see cref="dFlags" />
         /// </summary>
@@ -350,15 +350,15 @@ public sealed partial class BSD : IPartition
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
         public uint[] d_spare;
         /// <summary><see cref="BSD.DISK_MAGIC" /> again</summary>
-        public uint d_magic2;
+        public uint   d_magic2;
         /// <summary>XOR of data</summary>
         public ushort d_checksum;
         /// <summary>How many partitions</summary>
         public ushort d_npartitions;
         /// <summary>Size of boot area in bytes</summary>
-        public uint d_bbsize;
+        public uint   d_bbsize;
         /// <summary>Maximum size of superblock in bytes</summary>
-        public uint d_sbsize;
+        public uint   d_sbsize;
         /// <summary>Partitions</summary>
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
         public BSDPartition[] d_partitions;
@@ -369,53 +369,53 @@ public sealed partial class BSD : IPartition
 #region Nested type: dType
 
     /// <summary>Drive type</summary>
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Names match the original BSD headers")]
     enum dType : ushort
     {
         /// <summary>SMD, XSMD</summary>
-        SMD = 1,
+        SMD    = 1,
         /// <summary>MSCP</summary>
-        MSCP = 2,
+        MSCP   = 2,
         /// <summary>Other DEC (rk, rl)</summary>
-        DEC = 3,
+        DEC    = 3,
         /// <summary>SCSI</summary>
-        SCSI = 4,
+        SCSI   = 4,
         /// <summary>ESDI</summary>
-        ESDI = 5,
+        ESDI   = 5,
         /// <summary>ST506 et al</summary>
-        ST506 = 6,
+        ST506  = 6,
         /// <summary>CS/80 on HP-IB</summary>
-        HPIB = 7,
+        HPIB   = 7,
         /// <summary>HP Fiber-link</summary>
-        HPFL = 8,
+        HPFL   = 8,
         /// <summary>Floppy</summary>
         FLOPPY = 10,
         /// <summary>Concatenated disk</summary>
-        CCD = 11,
+        CCD    = 11,
         /// <summary>uvnode pseudo-disk</summary>
-        VND = 12,
+        VND    = 12,
         /// <summary>DiskOnChip</summary>
-        DOC2K = 13,
+        DOC2K  = 13,
         /// <summary>ATAPI</summary>
-        ATAPI = 13,
+        ATAPI  = 13,
         /// <summary>CMU RAIDframe</summary>
-        RAID = 14,
+        RAID   = 14,
         /// <summary>Logical disk</summary>
-        LD = 15,
+        LD     = 15,
         /// <summary>IBM JFS 2</summary>
-        JFS2 = 16,
+        JFS2   = 16,
         /// <summary>Cryptographic pseudo-disk</summary>
-        CGD = 17,
+        CGD    = 17,
         /// <summary>Vinum volume</summary>
-        VINUM = 18,
+        VINUM  = 18,
         /// <summary>Flash memory devices</summary>
-        FLASH = 19,
+        FLASH  = 19,
         /// <summary>Device-mapper pseudo-disk devices</summary>
-        DM = 20,
+        DM     = 20,
         /// <summary>Rump virtual disk</summary>
-        RUMPD = 21,
+        RUMPD  = 21,
         /// <summary>Memory disk</summary>
-        MD = 22
+        MD     = 22
     }
 
 #endregion
@@ -423,84 +423,84 @@ public sealed partial class BSD : IPartition
 #region Nested type: fsType
 
     /// <summary>Filesystem type</summary>
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
-    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Names match the original BSD headers")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Members document the full on-disk layout")]
     internal enum fsType : byte
     {
         /// <summary>Unused entry</summary>
-        Unused = 0,
+        Unused     = 0,
         /// <summary>Swap partition</summary>
-        Swap = 1,
+        Swap       = 1,
         /// <summary>UNIX 6th Edition</summary>
-        V6 = 2,
+        V6         = 2,
         /// <summary>UNIX 7th Edition</summary>
-        V7 = 3,
+        V7         = 3,
         /// <summary>UNIX System V</summary>
-        SystemV = 4,
+        SystemV    = 4,
         /// <summary>UNIX 7th Edition with 1K blocks</summary>
-        V7_1K = 5,
+        V7_1K      = 5,
         /// <summary>UNIX 8th Edition with 4K blocks</summary>
-        V8 = 6,
+        V8         = 6,
         /// <summary>4.2BSD Fast File System</summary>
-        BSDFFS = 7,
+        BSDFFS     = 7,
         /// <summary>MS-DOS filesystem</summary>
-        MSDOS = 8,
+        MSDOS      = 8,
         /// <summary>4.4LFS</summary>
-        BSDLFS = 9,
+        BSDLFS     = 9,
         /// <summary>In use, unknown or unsupported</summary>
-        Other = 10,
+        Other      = 10,
         /// <summary>HPFS</summary>
-        HPFS = 11,
+        HPFS       = 11,
         /// <summary>ISO9660</summary>
-        ISO9660 = 12,
+        ISO9660    = 12,
         /// <summary>Boot partition</summary>
-        Boot = 13,
+        Boot       = 13,
         /// <summary>Amiga FFS</summary>
-        AFFS = 14,
+        AFFS       = 14,
         /// <summary>Apple HFS</summary>
-        HFS = 15,
+        HFS        = 15,
         /// <summary>Acorn ADFS</summary>
-        FileCore = 16,
+        FileCore   = 16,
         /// <summary>Digital Advanced File System</summary>
-        ADVfs = 16,
+        ADVfs      = 16,
         /// <summary>Digital LSM Public Region</summary>
-        LSMpublic = 17,
+        LSMpublic  = 17,
         /// <summary>Linux ext2</summary>
-        ext2 = 17,
+        ext2       = 17,
         /// <summary>Digital LSM Private Region</summary>
         LSMprivate = 18,
         /// <summary>NTFS</summary>
-        NTFS = 18,
+        NTFS       = 18,
         /// <summary>Digital LSM Simple Disk</summary>
-        LSMsimple = 19,
+        LSMsimple  = 19,
         /// <summary>RAIDframe component</summary>
-        RAID = 19,
+        RAID       = 19,
         /// <summary>Concatenated disk component</summary>
-        CCD = 20,
+        CCD        = 20,
         /// <summary>IBM JFS2</summary>
-        JFS2 = 21,
+        JFS2       = 21,
         /// <summary>Apple UFS</summary>
-        AppleUFS = 22,
+        AppleUFS   = 22,
         /// <summary>Hammer filesystem</summary>
-        HAMMER = 22,
+        HAMMER     = 22,
         /// <summary>Hammer2 filesystem</summary>
-        HAMMER2 = 23,
+        HAMMER2    = 23,
         /// <summary>UDF</summary>
-        UDF = 24,
+        UDF        = 24,
         /// <summary>System V Boot filesystem</summary>
-        SysVBoot = 25,
+        SysVBoot   = 25,
         /// <summary>EFS</summary>
-        EFS = 26,
+        EFS        = 26,
         /// <summary>ZFS</summary>
-        ZFS = 27,
+        ZFS        = 27,
         /// <summary>NiLFS</summary>
-        NILFS = 27,
+        NILFS      = 27,
         /// <summary>Cryptographic disk</summary>
-        CGD = 28,
+        CGD        = 28,
         /// <summary>MINIX v3</summary>
-        MINIX = 29,
+        MINIX      = 29,
         /// <summary>FreeBSD nandfs</summary>
-        NANDFS = 30
+        NANDFS     = 30
     }
 
 #endregion
